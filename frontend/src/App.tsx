3021911b9e9927b621650c40476a36dccca0a8b7
1119wj <1119wj@naver.com>
2024-11-04 00:22:00 +0900

    feat: google map 컴포넌트 로드 테스트 (#12)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import './App.css';
-
-function AppContent() {
-  return (
-    <div className="App">
-      <p className={'text-xl text-blue-600 underline'}>Daily Load</p>
-    </div>
-  );
-}
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
+import GoogleMap from './GoogleMap';
+const render = (status: Status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <>로딩중...</>;
+    case Status.FAILURE:
+      return <>에러</>;
+    case Status.SUCCESS:
+      return <GoogleMap />;
+  }
+};
 
 function App() {
-  return <AppContent />;
+  return (
+    <Wrapper
+      apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY || ''}
+      render={render}
+    />
+  );
 }
 
 export default App;
