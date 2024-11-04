7dd15a3145c601cff19917320efb3cda37ad4d32
1119wj <1119wj@naver.com>
2024-11-04 21:24:06 +0900

    feat: googleMap 컴포넌트 구현 #12

--- frontend/src/GoogleMap.tsx
+++ frontend/src/GoogleMap.tsx
@@ -1,39 +1,19 @@
 import { useEffect, useRef, useState } from 'react';
+import { getGoogleMapStore, useGoogleMapStore } from './store/googleMapState';
 
-function GoogleMap() {
+const GoogleMap = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const [googleMap, setGoogleMap] = useState<google.maps.Map>();
-
+  const setGoogleMap = useGoogleMapStore((state) => state.setGoogleMap);
+  const googleMap = useGoogleMapStore((state) => state.googleMap);
   useEffect(() => {
-    if (ref.current) {
-      const initialMap = new window.google.maps.Map(ref.current, {
-        center: {
-          lat: 37.5,
-          lng: 127.0,
-        },
-        zoom: 16,
-        mapId: import.meta.env.VITE_GOOGLE_MAPS_ID,
-        disableDefaultUI: true,
-        clickableIcons: false,
-        minZoom: 10,
-        maxZoom: 18,
-        gestureHandling: 'greedy',
-        restriction: {
-          latLngBounds: {
-            north: 39,
-            south: 32,
-            east: 132,
-            west: 124,
-          },
-          strictBounds: true,
-        },
-      });
-
+    if (ref.current && !googleMap) {
+      const initialMap = getGoogleMapStore();
       setGoogleMap(initialMap);
+      ref.current.appendChild(initialMap.getDiv());
     }
-  }, []);
+  }, [googleMap]);
 
   return <div ref={ref} id="map" style={{ minHeight: '100vh' }} />;
-}
+};
 
 export default GoogleMap;
