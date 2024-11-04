019f02f52ad9b92956b57dfcf7e33fea5d01d3f4
Soap <miensop51@gmail.com>
2024-11-04 13:57:05 +0900

    test. AppController 테스트 수정 #30

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      expect(await appController.getHello()).toBe(
+        'Hello World!' + ' Web 09 Backend',
+      );
     });
   });
 });
