f135528910e8332ad6498448e85a178e84bbe9a0
1119wj <1119wj@naver.com>
2024-11-04 21:24:42 +0900

    feat: 대시보드 및 marker 컴포넌트 구현 #12

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Status, Wrapper } from '@googlemaps/react-wrapper';
 import GoogleMap from './GoogleMap';
+import Dashboard from './Dashboard';
 const render = (status: Status) => {
   switch (status) {
     case Status.LOADING:
@@ -8,15 +9,22 @@ const render = (status: Status) => {
     case Status.FAILURE:
       return <>에러</>;
     case Status.SUCCESS:
-      return <GoogleMap />;
+      return (
+        <>
+          <GoogleMap />
+          <Dashboard />
+        </>
+      );
   }
 };
 
 function App() {
+  const googleMapsApiUrl = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&v=3.49&libraries=marker`;
   return (
     <Wrapper
       apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY || ''}
       render={render}
+      libraries={['marker']}
     />
   );
 }
---
+++ frontend/src/marker.ts
@@ -0,0 +1,12 @@
+import { useGoogleMapStore } from "./store/googleMapState";
+
+export const createMarkerInstance = (latitude: number, longitude: number) => {
+    const googleMap = useGoogleMapStore.getState().googleMap;
+
+    const markerInstance = new google.maps.marker.AdvancedMarkerElement({
+      position: new google.maps.LatLng(latitude, longitude),
+      map: googleMap,
+    });
+  
+    return markerInstance;
+}
