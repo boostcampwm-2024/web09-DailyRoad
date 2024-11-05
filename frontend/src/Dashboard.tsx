04c5408cda0594b8d01485fdd9da34183b837b7c
1119wj <1119wj@naver.com>
2024-11-05 21:27:43 +0900

    feat: google 지도 라이브러리 변경 #26

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,41 +1,43 @@
 import { useEffect, useRef, useState } from 'react';
-import { getGoogleMapStore, useGoogleMapStore } from './store/googleMapState';
+import { useGoogleMapStore } from './store/googleMapState';
 import { createMarkerInstance } from './marker';
 import NewPlaceSearchBar from './NewPlaceSearchBar';
 
 const Dashboard = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const setGoogleMap = useGoogleMapStore((state) => state.setGoogleMap);
   const googleMap = useGoogleMapStore((state) => state.googleMap);
+  const initializeMap = useGoogleMapStore((state) => state.initializeMap);
+
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
   const [zoom, setZoom] = useState(0);
 
   useEffect(() => {
     if (ref.current && !googleMap) {
-      const initialMap = getGoogleMapStore();
-      setGoogleMap(initialMap);
-      ref.current.appendChild(initialMap.getDiv());
+      initializeMap(ref.current);
     }
-  }, []);
+  }, [googleMap, initializeMap]);
+
   useEffect(() => {
     if (googleMap) {
       const updateMapInfo = () => {
-        const center = googleMap.getCenter();
-        if (center) {
-          setCenter({ lat: center.lat(), lng: center.lng() });
+        const newCenter = googleMap.getCenter();
+        if (newCenter) {
+          setCenter({ lat: newCenter.lat(), lng: newCenter.lng() });
         }
         setZoom(googleMap.getZoom() || 0);
       };
 
       updateMapInfo();
 
-      googleMap.addListener('center_changed', updateMapInfo);
-
-      googleMap.addListener('zoom_changed', updateMapInfo);
+      const centerListener = googleMap.addListener(
+        'center_changed',
+        updateMapInfo,
+      );
+      const zoomListener = googleMap.addListener('zoom_changed', updateMapInfo);
 
       return () => {
-        google.maps.event.clearListeners(googleMap, 'center_changed');
-        google.maps.event.clearListeners(googleMap, 'zoom_changed');
+        google.maps.event.removeListener(centerListener);
+        google.maps.event.removeListener(zoomListener);
       };
     }
   }, [googleMap]);
@@ -45,8 +47,8 @@ const Dashboard = () => {
     setCenter((prev) => ({ ...prev, lat }));
     if (googleMap) {
       googleMap.setCenter({ lat, lng: center.lng });
+      createMarkerInstance(lat, center.lng);
     }
-    const marker = createMarkerInstance(lat, center.lng);
   };
 
   const handleLngChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -54,11 +56,12 @@ const Dashboard = () => {
     setCenter((prev) => ({ ...prev, lng }));
     if (googleMap) {
       googleMap.setCenter({ lat: center.lat, lng });
+      createMarkerInstance(center.lat, lng);
     }
   };
 
   return (
-    <div className={'absolute left-0 top-0 h-72 w-36 bg-white'}>
+    <div className="absolute left-0 top-0 h-72 w-36 bg-white">
       <NewPlaceSearchBar />
       <div>
         <label>
@@ -67,14 +70,19 @@ const Dashboard = () => {
             type="number"
             value={center.lat}
             onChange={handleLatChange}
-            step={'0.01'}
+            step="0.01"
           />
         </label>
       </div>
       <div>
         <label>
           Longitude:
-          <input value={center.lng} onChange={handleLngChange} />
+          <input
+            type="number"
+            value={center.lng}
+            onChange={handleLngChange}
+            step="0.01"
+          />
         </label>
       </div>
       <div>Zoom: {zoom}</div>
