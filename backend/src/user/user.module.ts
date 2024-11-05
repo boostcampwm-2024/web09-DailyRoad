1cb6afb359c14986c7883024f0163edabe4da22e
miensoap <miensop51@gmail.com>
2024-11-05 13:37:51 +0900

    feat: 임시 유저 모듈 구현 #21

---
+++ backend/src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+// Todo. 유저 기능 완성 후 교체
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule.forFeature([User])],
+})
+export class UserModule {
+}
