01a18fbe6765a30e44985dacc2fb70b496dd6aab
koomchang <leahpar0401@gmail.com>
2024-11-05 11:30:32 +0900

    feat: 장소 생성 api 구현 #38

---
+++ backend/src/place/place.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PlaceRepository } from './place.repository';
+import { CreatePlaceDto } from './dto/CreatePlaceDto';
+
+@Injectable()
+export class PlaceService {
+  constructor(private readonly placeRepository: PlaceRepository) {}
+
+  async addPlace(createPlaceDto: CreatePlaceDto) {
+    const savedPlace = await this.placeRepository.save(createPlaceDto);
+    return { id: savedPlace.id };
+  }
+}
