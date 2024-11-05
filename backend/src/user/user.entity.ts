4ba35eb9b3a78efcb6c6741e76b8b655e2242739
miensoap <miensop51@gmail.com>
2024-11-05 13:37:51 +0900

    feat: 임시 유저 모듈 구현 #21

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, OneToMany } from 'typeorm';
 import { BaseEntity } from '../common/BaseEntity';
 import { Course } from '../course/course.entity';
-import { Map } from '../map/map.entity';
+import { Map } from '../map/entity/map.entity';
 
 @Entity()
 export class User extends BaseEntity {
@@ -25,4 +25,16 @@ export class User extends BaseEntity {
 
   @OneToMany(() => Course, (course) => course.user)
   courses: Promise<Course[]>;
+
+  constructor(provider: string,
+              nickname: string,
+              oauthId: string, role: string,
+              profileImageUrl?: string) {
+    super();
+    this.provider = provider;
+    this.nickname = nickname;
+    this.oauthId = oauthId;
+    this.role = role;
+    this.profileImageUrl = profileImageUrl;
+  }
 }
