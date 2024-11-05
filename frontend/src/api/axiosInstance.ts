a1d0a652c54c074a5c3dc02c9a9fc54cd909eaa2
1119wj <1119wj@naver.com>
2024-11-05 21:28:56 +0900

    feat: axios instance 구현 #25

---
+++ frontend/src/api/axiosInstance.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+import { AXIOS_BASE_URL, NETWORK } from '@/constants/api';
+
+export const axiosInstance = axios.create({
+  baseURL: AXIOS_BASE_URL,
+  timeout: NETWORK.TIMEOUT,
+  withCredentials: true,
+  useAuth: true,
+});
