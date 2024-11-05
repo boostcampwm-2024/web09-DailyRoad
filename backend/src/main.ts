4b431280a44597bde3abe10cb31901c4bc9136ba
koomchang <leahpar0401@gmail.com>
2024-11-05 18:28:51 +0900

    fix: globalpipe를 지워버린 것을 복구 #45

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,8 @@ import { GlobalExceptionFilter } from './common/exception/filter/GlobalException
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
     origin: 'http://localhost:5173',
   });
