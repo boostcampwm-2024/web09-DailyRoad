ac329e3b6a6c6365a198d62ff8097eebd0e4b6bd
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-06 11:07:54 +0900

    feat: 회원 저장 기능 구현

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,11 +1,14 @@
-import { User } from './user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
 
 // Todo. 유저 기능 완성 후 교체
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  exports: [TypeOrmModule.forFeature([User])],
+  // imports: [TypeOrmModule.forFeature([User])],
+  // exports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+  providers: [UserService, UserRepository],
+  exports: [UserService, UserRepository],
 })
-export class UserModule {
-}
+export class UserModule {}
