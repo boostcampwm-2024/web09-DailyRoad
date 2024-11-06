e0ac1da0ce2a684995e73e83ea891cde57f89599
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-06 19:15:38 +0900

    refactor: 타입 파일 분리 #8

---
+++ backend/src/auth/authType.ts
@@ -0,0 +1,10 @@
+export type googleTokenResponse = {
+  access_token: string;
+  refresh_token: string;
+};
+
+export type googleUserResponse = {
+  id: string;
+  picture: string;
+  name: string;
+};
