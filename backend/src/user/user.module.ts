ce666c8f143744070052d21cdb08ae74b7f90f16
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-06 19:21:35 +0900

    refactor: 의미없는 주석 제거 #8

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -5,8 +5,6 @@ import { UserRepository } from './user.repository';
 
 // Todo. 유저 기능 완성 후 교체
 @Module({
-  // imports: [TypeOrmModule.forFeature([User])],
-  // exports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService, UserRepository],
