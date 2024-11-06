47c20645270c63bb3b887a0d302cf6e7df1b86bd
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-06 11:07:54 +0900

    feat: 회원 저장 기능 구현

---
+++ backend/src/user/user.repository.ts
@@ -0,0 +1,39 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+import { UserIconResponse } from './dto/UserIconResponse';
+
+@Injectable()
+export class UserRepository {
+  private readonly repository: Repository<User>;
+
+  constructor(dataSource: DataSource) {
+    this.repository = dataSource.getRepository(User);
+  }
+
+  async findOne(id: number): Promise<UserIconResponse | undefined> {
+    const user = await this.repository.findOne({ where: { id } });
+    return UserIconResponse.from(user);
+  }
+
+  async findByProviderAndOauthId(
+    provider: string,
+    oauthId: string,
+  ): Promise<User | undefined> {
+    return this.repository.findOne({ where: { provider, oauthId } });
+  }
+
+  async create(userData: Partial<User>): Promise<User> {
+    return this.repository.create({
+      provider: userData.provider,
+      oauthId: userData.oauthId,
+      nickname: userData.nickname,
+      role: userData.role,
+      profileImageUrl: userData.profileImageUrl,
+    });
+  }
+
+  async save(user: User) {
+    return this.repository.save(user);
+  }
+}
