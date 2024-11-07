c4c392d64e8ee083570b0e4f39000fff2de5c2bf
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-07 16:41:37 +0900

    feat:  CreateUserRequest dto 생성 #8

---
+++ backend/src/user/dto/CreateUserRequest.ts
@@ -0,0 +1,43 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { User } from '../user.entity';
+import { userInfoWithProvider } from '../userType';
+
+export class CreateUserRequest {
+  @IsString()
+  @IsNotEmpty()
+  provider: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nickname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  oauthId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  role: string;
+
+  @IsString()
+  @IsOptional()
+  profileImageUrl?: string;
+
+  constructor(user: userInfoWithProvider) {
+    this.provider = user.provider;
+    this.nickname = user.name;
+    this.oauthId = user.oauthId;
+    this.role = user.role;
+    this.profileImageUrl = user.picture;
+  }
+
+  toEntity(): User {
+    return new User(
+      this.provider,
+      this.nickname,
+      this.oauthId,
+      this.role,
+      this.profileImageUrl,
+    );
+  }
+}
