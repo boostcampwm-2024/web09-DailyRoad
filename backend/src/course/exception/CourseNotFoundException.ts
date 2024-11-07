7ddad61e3ea781ea673fa339a2960b1ddb5e4183
miensoap <miensop51@gmail.com>
2024-11-07 12:10:13 +0900

    feat: 코스 장소 추가 관련 예외 추가 #65

--- backend/src/course/exception/CourseNotFoundException.ts
+++ backend/src/course/exception/CourseNotFoundException.ts
@@ -4,7 +4,7 @@ import { HttpStatus } from '@nestjs/common';
 export class CourseNotFoundException extends BaseException {
   constructor(id: number) {
     super({
-      code: 802,
+      code: 902,
       message: `id:${id} 코스가 존재하지 않거나 삭제되었습니다.`,
       status: HttpStatus.NOT_FOUND,
     });
