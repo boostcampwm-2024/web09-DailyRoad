1beb55f85b8abbc0c249beaf5c4793324800781d
koomchang <leahpar0401@gmail.com>
2024-11-07 20:16:22 +0900

    refactor: place entity를 place/entity 내부로 경로 변경 #66

---
+++ backend/src/course/course.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { BaseEntity } from '../common/BaseEntity';
+import { User } from '../user/entity/user.entity';
+import { Place } from '../place/entity/place.entity';
+import { CoursePlace } from './course-place.entity';
+
+@Entity()
+export class Course extends BaseEntity {
+  @ManyToOne(() => User, (user) => user.courses)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column({ nullable: true })
+  thumbnailUrl?: string;
+
+  @Column()
+  title: string;
+
+  @Column('text', { nullable: true })
+  description?: string;
+
+  @OneToMany(() => CoursePlace, (coursePlace) => coursePlace.course)
+  private coursePlaces: Promise<CoursePlace[]>;
+
+  async getPlaces(): Promise<Place[]> {
+    const mapPlaces = await this.coursePlaces;
+    return await Promise.all(mapPlaces.map((coursePlace) => coursePlace.place));
+  }
+}
