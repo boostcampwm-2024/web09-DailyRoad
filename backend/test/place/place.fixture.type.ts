695b90147b9d13422c27f8bd10e5887aae725fc3
koomchang <leahpar0401@gmail.com>
2024-11-07 20:16:04 +0900

    refactor: PlaceFixture의 타입을 분리하여 재사용성 높임 #66

---
+++ backend/test/place/place.fixture.type.ts
@@ -0,0 +1,11 @@
+export type PlaceFixtureType = {
+  googlePlaceId?: string;
+  name?: string;
+  imageUrl?: string;
+  rating?: number;
+  longitude?: number;
+  latitude?: number;
+  formattedAddress?: string;
+  description?: string;
+  url?: string;
+};
