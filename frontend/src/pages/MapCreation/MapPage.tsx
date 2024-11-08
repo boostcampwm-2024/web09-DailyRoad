55f89f1a3e222d48d332c6c339cb5faf044e9da7
1119wj <1119wj@naver.com>
2024-11-08 22:50:47 +0900

    feat: 쿼리 프로바이더, 라우터 설정 #16

---
+++ frontend/src/pages/MapCreation/MapPage.tsx
@@ -0,0 +1,13 @@
+import Dashboard from '@/Dashboard';
+import GoogleMap from '@/GoogleMap';
+
+const MapPage = () => {
+  return (
+    <>
+      <GoogleMap />
+      <Dashboard />
+    </>
+  );
+};
+
+export default MapPage;
