322db06f96586db58de65c4b1bbec8a17aab6620
1119wj <1119wj@naver.com>
2024-11-08 22:42:53 +0900

    feat: 초기 지도 정보 생성 form 구현 #16

--- frontend/src/components/Form/CreateForm.tsx
+++
@@ -1,8 +0,0 @@
-import { createMap } from '@/api/map';
-import FormWrapper from './FormWrapper';
-
-const CreateForm = () => {
-  return <FormWrapper header="지도 생성" onSubmitHandler={createMap} />;
-};
-
-export default CreateForm;
