eecf3a9c5ada7bd177e4ed4c1b9af0ce244e88b1
1119wj <1119wj@naver.com>
2024-11-08 22:42:04 +0900

    feat: 간단한 메인페이지 구현 #16

---
+++ frontend/src/pages/Homepage/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto flex items-center justify-between px-6 py-4">
+        <h1 className="text-2xl font-bold">Logo</h1>
+        <button className="">로그인</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Homepage/HomePage.tsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Header from './Header';
+import NavigateButton from '@/components/common/NavigateButton';
+
+const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <NavigateButton text="나만의 지도 만들러 가기" to="/map" />
+      <Footer />
+    </>
+  );
+};
+
+export default Homepage;
