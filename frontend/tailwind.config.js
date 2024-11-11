d1236150506d040c29c1c38e7e4f6a1cd6110a94
1119wj <1119wj@naver.com>
2024-11-11 22:08:35 +0900

    feat: 아이콘 컴포넌트 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        c_button_gray: '#cdced6',
+        c_textarea_gray:'f9f9f9',
+        c_background_gray: '#fffff',
+        c_placeholder_gray:'#858899',
+        c_strong_black: '#000000',
+        c_description_gray: '#3E404c',
+        c_bg_blue: '#00a3ff',
+    },
   },
   plugins: [require('tailwind-scrollbar')],
   variants: {
     scrollbar: ['rounded'],
   },
-};
+}};
