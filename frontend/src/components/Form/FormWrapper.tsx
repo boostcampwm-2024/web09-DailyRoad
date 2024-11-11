8f070c090ca0ed1b2fd4c4f7c7ad25f54e21f64c
1119wj <1119wj@naver.com>
2024-11-11 22:09:35 +0900

    feat: css 수정 및 submit api 연동

--- frontend/src/components/Form/FormWrapper.tsx
+++ frontend/src/components/Form/FormWrapper.tsx
@@ -1,6 +1,10 @@
 import { BaseMapType, CreateMapType } from '@/types';
 import { useState } from 'react';
 import ImageUploader from './ImageUploader';
+import TextInputArea from '../common/TextInputArea';
+import VisibilitySelector from './VisibilitySelector';
+import { useNavigate } from 'react-router-dom';
+import Box from '../common/Box';
 
 type FormWrapperProps = {
   header: string;
@@ -18,74 +22,77 @@ const FormWrapper = ({
     initialData?.description || '',
   );
   const [thumbnailUrl, setThumbnailUrl] = useState(
-    initialData?.thumbnailUrl || '',
+    initialData?.thumbnailUrl || "https://example.com/map7.jpg",
   );
   const [isPublic, setIsPublic] = useState(initialData?.isPublic || false);
   const [mode, setMode] = useState<CreateMapType>('MAP');
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmitHandler({ title, description, isPublic });
   };
 
-  const navigateByMode = (mode: CreateMapType) => {};
+  const navigateByMode = (mode: CreateMapType) => {
+    onSubmitHandler({ title, description, isPublic,thumbnailUrl });
+    if (mode === 'MAP') {
+      return navigate('/map/map');
+    
+
+    } else {
+      return navigate('/map/course');
+    }
+  };
 
   return (
-    <div className="form-container rounded bg-white p-4 shadow-md">
-      <h2 className="mb-4 text-xl font-semibold">{header}</h2>
-      <div className="mb-4">
-        <label className="block text-gray-700">Mode:</label>
+    <div className='h-full'>
+      <h2 className="p-4 text-xl font-semibold">{header}</h2>
+      <div className="flex h-[200px] gap-4 p-4">
+        <ImageUploader/>
         <select
           value={mode}
           onChange={(e) => setMode(e.target.value as CreateMapType)}
-          className="w-full rounded border px-3 py-2"
+          className="w-[144px] h-[41px] rounded border px-3 py-2"
         >
-          <option value="map">Map</option>
-          <option value="course">Course</option>
+          <option value="map">지도</option>
+          <option value="course">코스</option>
         </select>
       </div>
 
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700">Title:</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="w-full rounded border px-3 py-2"
-            required
+      <form className='flex flex-col gap-4 bg-gray-100'>
+        <Box>
+        <TextInputArea 
+          value={title}
+          onChange={(prev)=>setTitle(prev)}
+          placeholder={"새 지도명을 입력해주세요."}
+          maxLength={20}
+          height={28}
+          isTextArea={false}
+        />
+        <p>지도 소개</p>
+        <TextInputArea
+          value={description}
+          onChange={(prev)=>setDescription(prev)}
+          placeholder={"지도 소개를 입력해주세요."}
+          maxLength={150}
+          height={160}
+          isTextArea={true}
           />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Description:</label>
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="w-full rounded border px-3 py-2"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <ImageUploader />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Visibility:</label>
-          <select
-            value={isPublic ? 'public' : 'private'}
-            onChange={(e) => setIsPublic(e.target.value === 'public')}
-            className="w-full rounded border px-3 py-2"
-          >
-            <option value="public">Public</option>
-            <option value="private">Private</option>
-          </select>
-        </div>
+        </Box>
+        
+        <Box>
+          <VisibilitySelector selected={isPublic ? 'public' : 'private'} onSelect={(selected)=>setIsPublic(selected==='public')}/>
+        </Box>
+      </form>
+      <Box>
         <button
           type="submit"
-          className="w-full rounded bg-blue-500 py-2 text-white hover:bg-blue-600"
+          className="w-full rounded bg-c_bg_blue text-white"
+          onClick={()=>navigateByMode(mode)}
         >
-          Submit
-        </button>
-      </form>
+          완료
+        </button> 
+      </Box>
     </div>
   );
 };
