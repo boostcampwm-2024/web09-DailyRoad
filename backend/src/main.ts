c928a87bebf124b9fbe31ded72cfcc80faa83b9b
Soap <miensop51@gmail.com>
2024-11-11 16:28:57 +0900

    refactor: 전역예외필터 타입별로 분리

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { GlobalExceptionFilter } from './common/exception/filter/GlobalExceptionFilter';
+import {
+  BaseExceptionFilter,
+  HttpExceptionFilter,
+  UnknownExceptionFilter,
+} from './common/exception/filter/GlobalExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalFilters(
+    new UnknownExceptionFilter(),
+    new HttpExceptionFilter(),
+    new BaseExceptionFilter(),
+  );
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(8080);
 }
