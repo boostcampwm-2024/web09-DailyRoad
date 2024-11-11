4eb070d4b0d9ff5787762d94ab133d79901edae1
Soap <miensop51@gmail.com>
2024-11-11 15:07:55 +0900

    fix: CORS 처리 Nginx로 이동

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,9 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: 'http://localhost:5173',
-  });
   await app.listen(8080);
 }
 
