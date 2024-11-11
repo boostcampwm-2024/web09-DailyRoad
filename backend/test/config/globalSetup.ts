eaee784b9e4725ae013bfa0dcf8000b906c6b7d8
koomchang <leahpar0401@gmail.com>
2024-11-12 01:47:26 +0900

    test: global 환경에서 생성 된 container를 재사용하여 테스트할 수 있도록 설정 #72

--- backend/test/config/globalSetup.ts
+++ backend/test/config/globalSetup.ts
@@ -5,9 +5,6 @@ export default async () => {
   const ddlScriptPath = path.resolve(__dirname, '../../resources/sql/DDL.sql');
 
   global.reusedContainer = await new MySqlContainer()
-    .withUsername('testUser')
-    .withUserPassword('testPassword')
-    .withDatabase('testDB')
     .withReuse()
     .withBindMounts([
       { source: ddlScriptPath, target: '/docker-entrypoint-initdb.d/DDL.sql' },
