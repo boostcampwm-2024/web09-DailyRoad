e18fab4b7a781970f9a0cd12d926de77be0df7f3
miensoap <miensop51@gmail.com>
2024-11-12 12:54:43 +0900

    feat: 액세스 토큰 갱신 API 구현 #89
     로그인시 쿠키에 리프레시 토큰 설정

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,37 +1,49 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Body, Controller, Post, Get, Param, Res, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserService } from '../user/user.service';
-import { CreateUserRequest } from '../user/dto/CreateUserRequest';
+import { getOAuthProviderNameByValue } from './oauthProvider/OAuthProvider';
+import { AuthenticationException } from './exception/AuthenticationException';
+
+const REFRESH_TOKEN = 'refreshToken';
 
 @Controller('oauth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Get('google/signIn')
-  async googleSignIn() {
-    return this.authService.getGoogleAuthUrl();
+  @Get(':provider/signIn')
+  async getSignInUrl(@Param('provider') provider: string) {
+    return this.authService.getSignInUrl(getOAuthProviderNameByValue(provider));
   }
 
-  @Post('google/signIn')
-  async googleCallback(@Body('code') code: string) {
-    const tokens = await this.authService.getGoogleToken(code);
-    const userInfo = await this.authService.getGoogleUserInfo(
-      tokens.accessToken,
+  @Post(':provider/signIn')
+  async handleCallback(
+    @Param('provider') provider: string,
+    @Body('code') code: string,
+    @Res() response: Response,
+  ) {
+    const tokens = await this.authService.signInWith(
+      getOAuthProviderNameByValue(provider),
+      code,
     );
-    const user = new CreateUserRequest({
-      ...userInfo,
-      provider: 'google',
-      role: 'member',
+
+    response.setHeader(
+      'Set-Cookie',
+      `${REFRESH_TOKEN}=${tokens.refreshToken}; HttpOnly; Path=/; SameSite=None; Secure;`,
+    );
+
+    response.json({
+      accessToken: tokens.accessToken,
     });
-    const { userId, role } = await this.userService.addUser(user);
-    return {
-      token: this.authService.generateJwt({
-        userId,
-        role,
-      }),
-    };
+  }
+
+  @Post('refresh')
+  async refreshAccessToken(@Req() req: Request) {
+    const refreshToken = req.cookies[REFRESH_TOKEN];
+    if (!refreshToken) {
+      throw new AuthenticationException('리프레시 토큰이 없습니다.');
+    }
+
+    const accessToken = await this.authService.refreshAccessToken(refreshToken);
+    return { accessToken };
   }
 }
