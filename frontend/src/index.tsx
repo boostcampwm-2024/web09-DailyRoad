880d204a4acbf9ee68afdfbde649898c324a80fa
1119wj <1119wj@naver.com>
2024-11-12 22:01:00 +0900

    style: 기타 lint 수정 #27

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
