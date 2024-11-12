09fac195c36dd8748c5a03e1c7404046d7eb3029
1119wj <1119wj@naver.com>
2024-11-12 22:01:31 +0900

    feat: 장소추가 api 구현 #27

--- frontend/src/api/place/index.ts
+++ frontend/src/api/place/index.ts
@@ -1,25 +1,30 @@
 import { END_POINTS } from '@/constants/api';
-import { Place } from '@/types';
+import { Place, PlaceMarker } from '@/types';
 import { axiosInstance } from '../axiosInstance';
 
 export const getPlace = async (queryString: string, pageParam: number) => {
-  const { data } = await axiosInstance.get<Place[]>(
-    END_POINTS.PLACE,
-    {
-      params: {
-        query: queryString,
-        page: pageParam,
-      },
+  const { data } = await axiosInstance.get<Place[]>(END_POINTS.PLACE, {
+    params: {
+      query: queryString,
+      page: pageParam,
     },
-  );
-  const Data =  data.map((place)=>{
+  });
+  const Data = data.map((place) => {
     return {
       ...place,
       location: {
         lat: Number(place.location.lat),
-        lng: Number(place.location.lng)
-      }
-    }
-  })
+        lng: Number(place.location.lng),
+      },
+    };
+  });
   return Data;
+};
+
+export const addPlace = async (placeMarkerData: PlaceMarker, mapId: number) => {
+  const { data } = await axiosInstance.post<PlaceMarker>(
+    END_POINTS.ADD_PLACE(mapId),
+    placeMarkerData,
+  );
+  return data;
 };
