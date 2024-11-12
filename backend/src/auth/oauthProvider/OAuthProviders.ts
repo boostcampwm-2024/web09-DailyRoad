6fb138e452b7d1347c71d85f70ecde78c65ff87a
miensoap <miensop51@gmail.com>
2024-11-12 15:32:49 +0900

    refactor: 지원하는 OAuth 제공자 관리 관련 코드 분리 #89

---
+++ backend/src/auth/oauthProvider/OAuthProviders.ts
@@ -0,0 +1,35 @@
+import { ConfigService } from '@nestjs/config';
+import { GoogleOAuthProvider } from './GoogleOAuthProvider';
+import { OAuthProvider } from './OAuthProvider';
+
+/**
+ * 지원하는 OAuth 제공자의 이름을 정의합니다.
+ */
+export enum OAuthProviders {
+  GOOGLE = 'google',
+}
+
+export type OAuthProviderName = keyof typeof OAuthProviders;
+
+/**
+ * 지원하는 OAuth 제공자 인스턴스들을 생성합니다.
+ * @param configService
+ */
+export function getOAuthProviders(
+  configService: ConfigService,
+): Record<OAuthProviderName, OAuthProvider> {
+  return {
+    GOOGLE: new GoogleOAuthProvider(configService),
+  };
+}
+
+/**
+ * OAuth 제공자의 이름을 찾아 반환합니다.
+ * @param value
+ */
+export function getOAuthProviderNameByValue(
+  value: string,
+): OAuthProviderName | undefined {
+  return (Object.entries(OAuthProviders).find(([, v]) => v === value) ||
+    [])[0] as OAuthProviderName | undefined;
+}
