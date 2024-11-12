eaaa693c471694fbc3ccd7f1d77efb671d346f5b
miensoap <miensop51@gmail.com>
2024-11-12 12:53:52 +0900

    refactor: provider 관련 로직 분리 #89

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { googleTokenResponse, googleUserResponse } from './auth.type';
 import { JWTHelper } from './JWTHelper';
+import { CreateUserRequest } from '../user/dto/CreateUserRequest';
+import { UserService } from '../user/user.service';
+import { RefreshTokenRepository } from './refresh-token.repository';
+import {
+  OAuthProviderName,
+  OAuthProvider,
+} from './oauthProvider/OAuthProvider';
+import { GoogleOAuthProvider } from './oauthProvider/GoogleOAuthProvider';
 import { AuthenticationException } from './exception/AuthenticationException';
-import { addBearerToken } from './utils';
+import { Role } from '../user/role.enum';
 
 @Injectable()
 export class AuthService {
-  private readonly clientId: string;
-  private readonly clientSecret: string;
-  private readonly redirectUri: string;
+  private readonly providers: Record<OAuthProviderName, OAuthProvider> = {
+    GOOGLE: new GoogleOAuthProvider(this.configService),
+  };
 
-    this.clientId = this.configService.get<string>('GOOGLE_CLIENT_ID');
-    this.clientSecret = this.configService.get<string>('GOOGLE_CLIENT_SECRET');
-    this.redirectUri = this.configService.get<string>('GOOGLE_REDIRECT_URI');
   private readonly accessTokenExpiration: string;
   private readonly refreshTokenExpiration: string;
+
   constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
     private readonly jwtHelper: JWTHelper,
+  ) {
     this.accessTokenExpiration = this.configService.get<string>(
       'ACCESS_TOKEN_EXPIRATION',
     );
@@ -26,47 +34,30 @@ export class AuthService {
     );
   }
 
-  getGoogleAuthUrl() {
-    const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
-    url.searchParams.append('client_id', this.clientId);
-    url.searchParams.append('redirect_uri', this.redirectUri);
-    url.searchParams.append('response_type', 'code');
-    url.searchParams.append('scope', 'openid profile');
-    return url.toString();
+  getSignInUrl(providerName: OAuthProviderName) {
+    const provider = this.getProvider(providerName);
+    return provider.getAuthUrl();
   }
 
-  async getGoogleToken(code: string): Promise<{ accessToken: string }> {
-    return fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        code: decodeURIComponent(code),
-        client_id: this.clientId,
-        client_secret: this.clientSecret,
-        redirect_uri: this.redirectUri,
-        grant_type: 'authorization_code',
-      }),
-    })
-      .then((response: Response) => response.json())
-      .then((data) => {
-        const tokens = {
-          access_token: data.access_token,
-          refresh_token: data.refresh_token,
-        };
-        if (!this.validateTokenResponse(tokens))
-          throw new AuthenticationException('Invalid Google Response');
-        return {
-          accessToken: tokens.access_token,
-        };
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
+  async signInWith(providerName: OAuthProviderName, code: string) {
+    const provider = this.getProvider(providerName);
+
+    const userInfo = await provider.getUserInfo(code);
+    const user = new CreateUserRequest({
+      ...userInfo,
+      provider: providerName,
+      role: Role.MEMBER,
+    });
+    const { userId, role } = await this.userService.addUser(user);
     return await this.generateTokens(userId, role);
   }
 
+  private getProvider(providerName: OAuthProviderName) {
+    const provider = this.providers[providerName];
+    if (!provider) {
+      throw new AuthenticationException('지원하지 않는 OAuth 제공자입니다');
+    }
+    return provider;
   }
 
   private async generateTokens(userId: number, role: string) {
@@ -95,28 +86,6 @@ export class AuthService {
   }
 
     });
-  }
 
-  async getGoogleUserInfo(accessToken: string) {
-    const url = 'https://www.googleapis.com/oauth2/v2/userinfo';
-    return fetch(url, {
-      headers: {
-        authorization: addBearerToken(accessToken),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (!this.validateUserInformationResponse(data)) {
-          throw new Error('Invalid Google User Information Response');
-        }
-        return {
-          oauthId: data?.id,
-          name: data?.name,
-          picture: data?.picture,
-        };
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
   }
 }
