f0837e1afe7af6896600b342f6b0dcb88b84017f
koomchang <leahpar0401@gmail.com>
2024-11-12 18:17:34 +0900

    refactor: 배너 리스트 응답 DTO 을 생성하여 응답 내용 변경 #70

---
+++ backend/src/banner/dto/ActiveBannerListResponse.ts
@@ -0,0 +1,23 @@
+import { Banner } from '../entity/banner.entity';
+
+export class ActiveBannerListResponse {
+  constructor(
+    readonly id: number,
+    readonly imageUrl: string,
+    readonly redirectUrl: string,
+    readonly startedAt: Date,
+    readonly endedAt: Date,
+  ) {}
+
+  static from(banner: Banner[]): ActiveBannerListResponse[] {
+    return banner.map((banner) => {
+      return new ActiveBannerListResponse(
+        banner.id,
+        banner.imageUrl,
+        banner.redirectUrl,
+        banner.startedAt,
+        banner.endedAt,
+      );
+    });
+  }
+}
