3818e8b2fb90817e81f5011527d97f00542b8867
1119wj <1119wj@naver.com>
2024-11-12 21:56:29 +0900

    fix: SearchPanel 컴포넌트 경로 수정 #27

---
+++ frontend/src/components/Place/SearchPanel.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import BaseWrapper from '../common/BaseWrapper';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+import Box from '../common/Box';
+import AddPlaceButton from './AddPlaceButton';
+
+const SearchPanel = () => {
+  const [query, setQuery] = useState('');
+
+  return (
+    <BaseWrapper>
+      <Box>
+        <h2 className="p-4 text-xl font-semibold">장소 검색</h2>
+        <SearchBar onSearch={(newQuery) => setQuery(newQuery)} />
+      </Box>
+      <Box>
+        <SearchResults query={query} />
+      </Box>
+      <AddPlaceButton />
+    </BaseWrapper>
+  );
+};
+
+export default SearchPanel;
