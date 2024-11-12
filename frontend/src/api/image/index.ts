0130024244c8e54c068736fac67f4182a41bb63b
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-12 19:09:23 +0900

    fix: 함수 내에서 파일의 확장자, 크기 검사 후 변경된 컨트롤러에 맞춰서 method POST 로 변경/ 이름 변경 #88

--- frontend/src/api/image/index.ts
+++ frontend/src/api/image/index.ts
@@ -1,31 +1,54 @@
 import { axiosInstance } from '../axiosInstance';
 import { PreSignedURLResponse } from '../../types';
-import { END_POINTS } from '@/constants/api';
+import { END_POINTS, IMAGE_EXTENSIONS } from '@/constants/api';
+import { THREE_MB } from '../../constants/api';
 
-const generatePreSignedUrl = async (dirName: string, extension: string) => {
-  const { data } = await axiosInstance.get<PreSignedURLResponse>(
-    END_POINTS.PRE_SIGNED_URL(dirName, extension),
+export const generatePreSignedPost = async (
+  dirName: string,
+  extension: string,
+) => {
+  const { data } = await axiosInstance.post<PreSignedURLResponse>(
+    END_POINTS.PRE_SIGNED_POST,
+    {
+      dirName: dirName,
+      extension: extension,
+    },
   );
   return data;
 };
 
-export const uploadImage = async (
-  file: File,
-  dirName: string,
-  extension: string,
-) => {
-  const preSignedURL = await generatePreSignedUrl(dirName, extension);
+export const getExtensionByFile = (file: File) => {
+  const extension = file.name.split('.').pop();
+  return extension ? extension.toLowerCase() : null;
+};
+
+export const validateFile = (file: File, extension: string | null) => {
+  return !(
+    !extension ||
+    !IMAGE_EXTENSIONS.has(extension) ||
+    file.size > THREE_MB
+  );
+};
+
+export const uploadImage = async (file: File, dirName: string) => {
+  const extension = getExtensionByFile(file);
+  if (!validateFile(file, extension)) {
+    throw new Error(
+      '지원되지 않는 파일 형식이거나 파일 크기가 3MB를 초과합니다.',
+    );
+  }
+  const preSignedPost = await generatePreSignedPost(dirName, extension!);
   const formData = new FormData();
-  Object.entries(preSignedURL.fields).forEach(([key, value]) => {
+  Object.entries(preSignedPost.fields).forEach(([key, value]) => {
     formData.append(key, value);
   });
   formData.append('file', file);
-  return fetch(preSignedURL.url, {
+  return fetch(preSignedPost.url, {
     method: 'POST',
     body: formData,
   })
     .then(() => {
-      return preSignedURL.uploadedUrl;
+      return preSignedPost.uploadedUrl;
     })
     .catch((err) => {
       throw new Error(err);
