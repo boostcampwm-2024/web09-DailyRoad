a3973773639cf49a7227cf5dcf9afb070cfec87d
koomchang <leahpar0401@gmail.com>
2024-11-13 03:40:19 +0900

    test: 테스트 코스를 위한 CourseFixture 구현 #92

---
+++ backend/test/course/fixture/course.fixture.type.ts
@@ -0,0 +1,9 @@
+import { User } from '../../../src/user/entity/user.entity';
+
+export type CourseFixtureType = {
+  user?: User;
+  title?: string;
+  isPublic?: boolean;
+  thumbnailUrl?: string;
+  description?: string;
+};
