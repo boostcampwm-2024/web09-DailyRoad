45c53cb7c701496669d8d9a6f18f08fdfe492690
miensoap <miensop51@gmail.com>
2024-11-12 12:52:44 +0900

    refactor: JWT 관련 로직 분리 #89
    AuthModule을 글로벌 모듈로 설정

---
+++ backend/src/auth/JWTHelper.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import * as jwt from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JWTHelper {
+  private readonly jwtSecretKey: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.jwtSecretKey = this.configService.get<string>('JWT_SECRET_KEY');
+  }
+
+  generateToken(payload: any, expiresIn: string): string {
+    return jwt.sign(payload, this.jwtSecretKey, {
+      expiresIn,
+    });
+  }
+
+  verifyToken(refreshToken: string) {
+    return jwt.verify(refreshToken, this.jwtSecretKey);
+  }
+}
