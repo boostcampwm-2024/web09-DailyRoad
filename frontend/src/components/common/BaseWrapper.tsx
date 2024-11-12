512dcff8a23a343d3f70f0db98646664f32a4d54
1119wj <1119wj@naver.com>
2024-11-12 21:54:43 +0900

    fix: 배경색 및 gap 추가 #27

--- frontend/src/components/common/BaseWrapper.tsx
+++ frontend/src/components/common/BaseWrapper.tsx
@@ -1,5 +1,5 @@
 const BaseWrapper = ({ children }: { children: React.ReactNode }) => (
-  <div className="absolute left-0 top-0 h-full w-[400px] rounded-lg bg-white shadow-md">
+  <div className="absolute left-0 top-0 flex h-full w-[400px] flex-col gap-0.5 rounded-lg bg-gray-200 shadow-md">
     {children}
   </div>
 );
