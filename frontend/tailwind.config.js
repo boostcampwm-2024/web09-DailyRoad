6eb825dff5dd32ca7d94553493390c0e59d81eca
1119wj <1119wj@naver.com>
2024-11-12 22:10:35 +0900

    style: 커스텀 css 추가 #27

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      width: {
+        inherit: 'inherit',
+      },
       colors: {
         c_button_gray: '#cdced6',
-        c_textarea_gray:'f9f9f9',
+        c_textarea_gray: 'f9f9f9',
         c_background_gray: '#fffff',
-        c_placeholder_gray:'#858899',
+        c_placeholder_gray: '#858899',
         c_strong_black: '#000000',
         c_description_gray: '#3E404c',
         c_bg_blue: '#00a3ff',
+      },
     },
+    plugins: [require('tailwind-scrollbar')],
   },
-  plugins: [require('tailwind-scrollbar')],
-  variants: {
-    scrollbar: ['rounded'],
-  },
-}};
+};
