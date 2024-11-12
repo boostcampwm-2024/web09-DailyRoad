2f85f444840d774e8c1802de887e80fbf11de122
1119wj <1119wj@naver.com>
2024-11-12 21:59:20 +0900

    feat: 지도 생성 Form 커스텀훅 구현 #27

---
+++ frontend/src/hooks/useMapFrom.ts
@@ -0,0 +1,36 @@
+import { BaseMap } from '@/types';
+import { useCallback, useEffect, useState } from 'react';
+
+const defualtMapData: BaseMap = {
+  title: '',
+  description: '',
+  thumbnailUrl: 'https://example.com/map7.jpg',
+  isPublic: true,
+  mode: 'MAP',
+};
+
+export const useMapForm = (initialMapData?: BaseMap) => {
+  const [mapInfo, setMapInfo] = useState<BaseMap>(
+    initialMapData ?? defualtMapData,
+  );
+  const [isMapInfoValid, setIsMapInfoValid] = useState(false);
+
+  useEffect(() => {
+    validateInputs();
+    console.log(isMapInfoValid);
+  }, [mapInfo]);
+
+  const validateInputs = () => {
+    const { title, description } = mapInfo;
+    setIsMapInfoValid(!!title && !!description);
+  };
+
+  const updateMapInfo = useCallback(
+    <K extends keyof BaseMap>(field: K, value: BaseMap[K]) => {
+      setMapInfo((prev) => ({ ...prev, [field]: value }));
+    },
+    [],
+  );
+
+  return { mapInfo, updateMapInfo, isMapInfoValid };
+};
