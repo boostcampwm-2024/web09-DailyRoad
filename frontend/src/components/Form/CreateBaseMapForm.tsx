5839a2ac8ae33c37854d41077369e9f695816a9e
1119wj <1119wj@naver.com>
2024-11-12 22:08:30 +0900

    feat: 지도 생성 컴포넌트 mutation 연동 #27

--- frontend/src/components/Form/CreateBaseMapForm.tsx
+++ frontend/src/components/Form/CreateBaseMapForm.tsx
@@ -1,11 +1,42 @@
-import { createMap } from '@/api/map';
 import FormWrapper from './FormWrapper';
-import BaseWrapper from '../BaseWrapper';
+import BaseWrapper from '../common/BaseWrapper';
+import { useNavigate } from 'react-router-dom';
+import { useAddMapMutation } from '@/hooks/api/useAddMapMutation';
+import { useMapForm } from '@/hooks/useMapFrom';
+import { CreateMapType } from '@/types';
 
 const CreateBaseMapForm = () => {
+  const { mapInfo, updateMapInfo, isMapInfoValid } = useMapForm();
+  const addMapMutation = useAddMapMutation();
+  const navigate = useNavigate();
+  const { mode, ...baseMap } = mapInfo;
+
+  const handlesubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addMapMutation.mutate(baseMap, {
+      onSuccess: () => {
+        navigateByMode(mode);
+      },
+    });
+  };
+
+  const navigateByMode = (mode: CreateMapType) => {
+    if (mode === 'MAP') {
+      return navigate('/create/map');
+    } else {
+      return navigate('/create/course');
+    }
+  };
+
   return (
     <BaseWrapper>
-      <FormWrapper header="새 지도/코스 추가" onSubmitHandler={createMap} />
+      <FormWrapper
+        header="새 지도/코스 추가"
+        mapInfo={mapInfo}
+        updateMapInfo={updateMapInfo}
+        isMapInfoValid={isMapInfoValid}
+        onSubmitHandler={handlesubmit}
+      />
     </BaseWrapper>
   );
 };
