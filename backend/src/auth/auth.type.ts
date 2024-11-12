1c71ea020b541d170c78385466a11774146997da
miensoap <miensop51@gmail.com>
2024-11-12 12:49:53 +0900

    refactor: 타입가드 함수 이동 #89

--- backend/src/auth/auth.type.ts
+++ backend/src/auth/auth.type.ts
@@ -2,8 +2,25 @@ export type googleTokenResponse = {
   access_token: string;
 };
 
+export function isGoogleTokenResponse(
+  response: any,
+): response is googleTokenResponse {
+  return response && typeof response.access_token === 'string';
+}
+
 export type googleUserResponse = {
   id: string;
   picture: string;
   name: string;
 };
+
+export function isGoogleUserResponse(
+  response: any,
+): response is googleUserResponse {
+  return (
+    response &&
+    typeof response.id === 'string' &&
+    typeof response.picture === 'string' &&
+    typeof response.name === 'string'
+  );
+}
