f0b9bb5fe7a0cc9f71f97f4434d2a4f422ac5fb3
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-12 19:07:46 +0900

    refactor: Pre-Signed Post 로 전체적으로 이름 변경/Storage 로 module, controller, service 이름 변경 / 유량 제어 추가 #88

---
+++ backend/src/storage/storage.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { StorageService } from './storage.service';
+import { JwtAuthGuard } from '../auth/JwtAuthGuard';
+import { PreSignedPostRequest } from './dto/PreSignedPostRequest';
+import { Throttle } from '@nestjs/throttler';
+
+@Controller('storage')
+export class StorageController {
+  constructor(private readonly storageService: StorageService) {}
+
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
+  @Post('/preSignedPost')
+  @UseGuards(JwtAuthGuard)
+  async getPreSignedPostUrl(
+    @Body() preSignedPostRequest: PreSignedPostRequest,
+  ) {
+    const { dirName, extension } = preSignedPostRequest;
+    return await this.storageService.generatePreSignedPost(dirName, extension);
+  }
+}
