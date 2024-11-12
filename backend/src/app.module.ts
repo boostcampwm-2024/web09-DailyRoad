c4179c203e8fed134ae4fb2966d148da0787573c
koomchang <leahpar0401@gmail.com>
2024-11-12 17:11:27 +0900

    feat: 배너 모듈 생성 #70

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,7 @@ import { PlaceModule } from './place/place.module';
 import { CourseModule } from './course/course.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BannerModule } from './banner/banner.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { UserModule } from './user/user.module';
     PlaceModule,
     MapModule,
     CourseModule,
+    BannerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
