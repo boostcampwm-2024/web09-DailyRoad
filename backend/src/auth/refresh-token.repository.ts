36e1fbadc90baac563812df006cd09cdeb3df691
miensoap <miensop51@gmail.com>
2024-11-12 12:49:19 +0900

    feat: 리프레시 토큰 엔티티/레포지토리 추가 #89

---
+++ backend/src/auth/refresh-token.repository.ts
@@ -0,0 +1,14 @@
+import { RefreshToken } from './entity/refresh-token.entity';
+import { Repository, DataSource } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RefreshTokenRepository extends Repository<RefreshToken> {
+  constructor(private dataSource: DataSource) {
+    super(RefreshToken, dataSource.createEntityManager());
+  }
+
+  async deleteByUserId(userId: number): Promise<void> {
+    await this.delete({ user: { id: userId } });
+  }
+}
