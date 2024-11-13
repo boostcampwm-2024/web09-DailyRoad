f83adc957e8a8879627cba900b112acf7038cd60
1119wj <1119wj@naver.com>
2024-11-13 21:51:46 +0900

    feat: 라우트 레이아웃 사용 #118

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,19 @@ import MapPage from './pages/MapCreation/MapPage';
 import MapCreateMapPage from './pages/MapCreation/MapCreateMapPage';
 import MapCreateCoursePage from './pages/MapCreation/MapCreateCoursePage';
 import PlaceCreatePage from './pages/PlaceCreation/PlaceCreatePage';
+import LayoutCreate from './LayoutCreate';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/create" element={<MapPage />} />
-        <Route path="/create/map" element={<MapCreateMapPage />} />
-        <Route path="/create/course" element={<MapCreateCoursePage />} />
-        <Route path="/create/map/:mapId" element={<PlaceCreatePage />} />
+        <Route path="/create" element={<LayoutCreate />}>
+          <Route index element={<MapPage />} />
+          <Route path="map" element={<MapCreateMapPage />} />
+          <Route path="course" element={<MapCreateCoursePage />} />
+          <Route path="map/:mapId" element={<PlaceCreatePage />} />
+        </Route>
       </Routes>
     </Router>
   );
