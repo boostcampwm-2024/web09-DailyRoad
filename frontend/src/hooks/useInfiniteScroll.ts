15336c10dc2b830e04df989fd0808c5e302b05ea
1119wj <1119wj@naver.com>
2024-11-13 16:43:22 +0900

    refactor: 타입 명확하게 명시 및 쿼리키 수정 #118

--- frontend/src/hooks/useInfiniteScroll.ts
+++ frontend/src/hooks/useInfiniteScroll.ts
@@ -2,8 +2,15 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { useInView } from 'react-intersection-observer';
 import { useEffect } from 'react';
 
+/**
+ * todo:
+ * 에러 발생 시 재시도 설정
+ * 데이터 캐싱 전략 검토
+ * 스크롤 위치 복원 로직 추가
+ */
+
 type InfiniteScrollOptions<TQueryFnData> = {
-  queryKey: string;
+  queryKey: string[];
   query?: string;
   queryFn: ({ pageParam }: { pageParam: number }) => Promise<TQueryFnData>;
   getNextPageParam: (
@@ -22,7 +29,7 @@ export const useInfiniteScroll = <TQueryFnData>({
 }: InfiniteScrollOptions<TQueryFnData>) => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useInfiniteQuery<TQueryFnData>({
-      queryKey: [queryKey, query],
+      queryKey: [...queryKey, query] as const,
       queryFn: ({ pageParam = 1 }) =>
         queryFn({ pageParam: pageParam as number }),
       initialPageParam: 1,
