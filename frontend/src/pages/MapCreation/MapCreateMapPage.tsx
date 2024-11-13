d5a0b15b8f90f10101810eef32105a9fafb6c2f5
1119wj <1119wj@naver.com>
2024-11-13 21:53:19 +0900

    feat: 레이아웃 수정 #118

--- frontend/src/pages/MapCreation/MapCreateMapPage.tsx
+++ frontend/src/pages/MapCreation/MapCreateMapPage.tsx
@@ -1,13 +1,7 @@
 import SearchPanel from '@/components/Place/SearchPanel';
-import GoogleMap from '@/GoogleMap';
 
 const MapCreateMapPage = () => {
-  return (
-    <>
-      <GoogleMap />
-      <SearchPanel />
-    </>
-  );
+  return <SearchPanel />;
 };
 
 export default MapCreateMapPage;
