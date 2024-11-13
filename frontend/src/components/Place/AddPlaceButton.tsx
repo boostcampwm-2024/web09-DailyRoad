1e51d59a5b99e68408bbac8b8614a162e7e4ef7f
1119wj <1119wj@naver.com>
2024-11-13 21:55:07 +0900

    feat: 장소추가 상세페이지로 이동 기능 #118

--- frontend/src/components/Place/AddPlaceButton.tsx
+++ frontend/src/components/Place/AddPlaceButton.tsx
@@ -1,17 +1,20 @@
 import { useStore } from '@/store/useStore';
 import Box from '../common/Box';
+import { Link } from 'react-router-dom';
 
 const AddPlaceButton = () => {
   const activePlace = useStore((state) => state.place);
   return (
-    <Box>
-      <button
-        className={`h-14 w-full rounded-md ${activePlace ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
-        disabled={!!!activePlace}
-      >
-        장소 추가하기
-      </button>
-    </Box>
+    <Link to={`/create/map/${activePlace.id}`}>
+      <Box>
+        <button
+          className={`h-14 w-full rounded-md ${activePlace ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
+          disabled={!!!activePlace}
+        >
+          장소 추가하기
+        </button>
+      </Box>
+    </Link>
   );
 };
 
