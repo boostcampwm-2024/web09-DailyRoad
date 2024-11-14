93f6ad06bf19f56c10fa8f4f7d5525a5271c7da8
1119wj <1119wj@naver.com>
2024-11-15 01:48:14 +0900

    feat: 색깔 선택 탭 구현 #79

---
+++ frontend/src/pages/PlaceCreation/ColorItem.tsx
@@ -0,0 +1,22 @@
+import { MarkerColor } from '@/types';
+import { markerBgColor } from '@/constants/map';
+import CheckIcon from '@/components/Place/CheckIcon';
+type ColorItemProps = {
+  color: MarkerColor;
+  activeColor: MarkerColor | null;
+  setActiveColor: (color: MarkerColor | null) => void;
+};
+
+const ColorItem = ({ color, activeColor, setActiveColor }: ColorItemProps) => {
+  const bgColor = markerBgColor[color];
+  return (
+    <div
+      onClick={() => setActiveColor(color)}
+      className={`${bgColor} flex h-11 w-11 cursor-pointer items-center justify-center rounded-md`}
+    >
+      {activeColor === color && <CheckIcon />}
+    </div>
+  );
+};
+
+export default ColorItem;
