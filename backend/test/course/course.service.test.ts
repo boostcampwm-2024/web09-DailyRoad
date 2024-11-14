4c343f0f30200af6fc3db5719bcd2ceddaf0051b
koomchang <leahpar0401@gmail.com>
2024-11-14 14:59:31 +0900

    test: 검색 쿼리가 있을 때 해당 공개 코스를 반환한다 #117

--- backend/test/course/course.service.test.ts
+++ backend/test/course/course.service.test.ts
@@ -11,13 +11,20 @@ describe('CourseService', () => {
   let courseService: CourseService;
   let courseRepository: Partial<Record<keyof CourseRepository, jest.Mock>>;
   let placeRepository: Partial<Record<keyof PlaceRepository, jest.Mock>>;
+  let fakeUser1: User;
+
+  beforeAll(async () => {
+    fakeUser1 = { id: 1 } as User;
+  });
 
   beforeEach(async () => {
     courseRepository = {
       save: jest.fn(),
       findAll: jest.fn(),
       findByUserId: jest.fn(),
+      searchByTitleQuery: jest.fn(),
       countAllPublic: jest.fn(),
+      countByTitleAndIsPublic: jest.fn(),
       countByUserId: jest.fn(),
       updateIsPublicById: jest.fn(),
       updateInfoById: jest.fn(),
@@ -48,7 +55,7 @@ describe('CourseService', () => {
       { title: 'Public Course 2', isPublic: true },
     ].map(({ title, isPublic }) =>
       CourseFixture.createCourse({
-        user: { id: 1 } as User,
+        user: fakeUser1,
         title,
         isPublic,
       }),
@@ -74,6 +81,46 @@ describe('CourseService', () => {
       pageSize,
     );
 
+    expect(result).toEqual(expectedResponse);
+  });
+
+  it('검색 쿼리가 있을 때 해당 공개 코스를 반환한다', async () => {
+    const publicCoursesWithFood = [
+      { title: 'Food Course 1', isPublic: true },
+      { title: 'Food Course 2', isPublic: true },
+    ].map(({ title, isPublic }) =>
+      CourseFixture.createCourse({
+        user: { id: 1 } as User,
+        title,
+        isPublic,
+      }),
+    );
+
+    courseRepository.searchByTitleQuery.mockResolvedValue(
+      publicCoursesWithFood,
+    );
+    courseRepository.countByTitleAndIsPublic.mockResolvedValue(
+      publicCoursesWithFood.length,
+    );
+
+    const page = 1;
+    const pageSize = 10;
+    const query = 'Food';
+    const result = await courseService.searchPublicCourses(
+      query,
+      page,
+      pageSize,
+    );
+
+    const expectedCourses = await Promise.all(
+      publicCoursesWithFood.map(CourseListResponse.from),
+    );
+    const expectedResponse = new PagedCourseResponse(
+      expectedCourses,
+      publicCoursesWithFood.length,
+      page,
+      pageSize,
+    );
     expect(result).toEqual(expectedResponse);
   });
 });
