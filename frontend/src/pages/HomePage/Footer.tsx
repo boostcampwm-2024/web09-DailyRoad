9e1758dc8424dfe97c9c7aeacd3f3bae396cd411
1119wj <1119wj@naver.com>
2024-11-15 01:50:37 +0900

    style: 메인페이지 UI 및 title #79

--- frontend/src/pages/HomePage/Footer.tsx
+++ frontend/src/pages/HomePage/Footer.tsx
@@ -3,19 +3,24 @@ const Footer = () => {
     <footer className="fixed bottom-0 w-full bg-white py-6 text-gray-400">
       <div className="container mx-auto flex flex-col items-center">
         <div className="mb-4 flex space-x-4">
-          <a href="/" className="hover:text-gray-400">
-            Privacy Policy
+          <a
+            href="https://github.com/boostcampwm-2024/web09-DailyRoad"
+            className="hover:text-gray-400"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
           </a>
-          <a href="/terms" className="hover:text-gray-400">
-            Terms of Service
-          </a>
-          <a href="/contact" className="hover:text-gray-400">
-            Contact Us
+          <a
+            href="https://elastic-bread-9ef.notion.site/12963e6f4ee98074b6f9f70cfa9ac836"
+            className="hover:text-gray-400"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Notion
           </a>
         </div>
-        <p className="text-sm">
-          &copy; 2024 Your Company. All rights reserved.
-        </p>
+        <p className="text-sm">2024 DailyRoad. All rights reserved.</p>
       </div>
     </footer>
   );
