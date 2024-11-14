0f8f471162c4a03d571fe163d24402f319f67956
1119wj <1119wj@naver.com>
2024-11-15 01:58:25 +0900

    feat: 커스텀 색 추가 및 폰트 #preview #79

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['pretendard'],
+      },
       width: {
         inherit: 'inherit',
       },
@@ -15,6 +18,12 @@ module.exports = {
         c_strong_black: '#000000',
         c_description_gray: '#3E404c',
         c_bg_blue: '#00a3ff',
+        c_marker_RED: '#FF8080',
+        c_marker_ORANGE: '#FFB780',
+        c_marker_YELLOW: '#FFE480',
+        c_marker_GREEN: '#7FC782',
+        c_marker_BLUE: '#80A6FF',
+        c_marker_PURPLE: '#DD80FF',
       },
     },
   },
