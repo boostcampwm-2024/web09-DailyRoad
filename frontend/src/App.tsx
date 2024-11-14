0a530f7ead7d554ffbedaf8212b15ff2513c3c8b
1119wj <1119wj@naver.com>
2024-11-14 10:19:25 +0900

    style: 경로 수정 #118

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/HomePage/HomePage';
-import MapPage from './pages/MapCreation/MapPage';
-import MapCreateMapPage from './pages/MapCreation/MapCreateMapPage';
-import MapCreateCoursePage from './pages/MapCreation/MapCreateCoursePage';
-import PlaceCreatePage from './pages/PlaceCreation/PlaceCreatePage';
-import LayoutCreate from './LayoutCreate';
+import Homepage from '@/pages/HomePage/HomePage';
+import MapPage from '@/pages/MapCreation/MapPage';
+import MapCreateMapPage from '@/pages/MapCreation/MapCreateMapPage';
+import MapCreateCoursePage from '@/pages/MapCreation/MapCreateCoursePage';
+import PlaceCreatePage from '@/pages/PlaceCreation/PlaceCreatePage';
+import LayoutCreate from '@/LayoutCreate';
 
 function App() {
   return (
