db9b7a4ba808d469347a636641658d0322103916
1119wj <1119wj@naver.com>
2024-11-15 01:56:24 +0900

    feat: icon 컴포넌트 #79

---
+++ frontend/src/components/Place/CheckIcon.tsx
@@ -0,0 +1,21 @@
+const CheckIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 84 68"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 32.3529L30.48 62L78 6"
+        stroke="white"
+        strokeWidth="12"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CheckIcon;
