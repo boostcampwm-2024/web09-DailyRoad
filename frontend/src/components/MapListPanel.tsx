092fa14db27402688f9aa86227a329615523281e
1119wj <1119wj@naver.com>
2024-11-15 01:49:06 +0900

    style: 버튼 위치 변경 및 css #79

--- frontend/src/components/MapListPanel.tsx
+++ frontend/src/components/MapListPanel.tsx
@@ -3,6 +3,7 @@ import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { MapItemType, MapList } from '@/types';
 import React from 'react';
 import MapItem from './MapItem';
+import NavigateButton from './common/NavigateButton';
 
 const MapListPanel = () => {
   const { data, isFetchingNextPage, hasNextPage, ref } =
@@ -17,18 +18,27 @@ const MapListPanel = () => {
       defaultFetch: true,
     });
   return (
-    <div className="scrollbar-thumb-rounded-lg flex max-h-[600px] flex-wrap space-y-4 overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-400 hover:scrollbar-track-gray-200 hover:scrollbar-thumb-gray-500">
-      {data?.pages.map((page, index) => (
-        <React.Fragment key={index}>
-          {page.maps.map((map: MapItemType) => (
-            <React.Fragment key={map.id}>
-              <MapItem key={map.id} mapItem={map} />
-            </React.Fragment>
-          ))}
-        </React.Fragment>
-      ))}
-      <div ref={ref} className="h-1" />
-    </div>
+    <>
+      <div className="flex h-8 w-auto justify-center">
+        <NavigateButton
+          to="/create"
+          text="지도/코스 추가"
+          className="w-30 h-10 rounded-md border-[1.5px] border-c_button_gray p-2"
+        />
+      </div>
+      <div className="scrollbar-thumb-rounded-lg flex h-full flex-wrap gap-4 overflow-y-auto p-20 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-400 hover:scrollbar-track-gray-200 hover:scrollbar-thumb-gray-500">
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.maps.map((map: MapItemType) => (
+              <React.Fragment key={map.id}>
+                <MapItem key={map.id} mapItem={map} />
+              </React.Fragment>
+            ))}
+          </React.Fragment>
+        ))}
+        <div ref={ref} className="h-1" />
+      </div>
+    </>
   );
 };
 
