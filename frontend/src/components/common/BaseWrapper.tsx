2cc93c25f83a1fc39f442c5309d24c90b18f3d21
1119wj <1119wj@naver.com>
2024-11-15 01:56:09 +0900

    style: 기타 css 수정 #79

--- frontend/src/components/common/BaseWrapper.tsx
+++ frontend/src/components/common/BaseWrapper.tsx
@@ -6,7 +6,7 @@ const BaseWrapper = ({
   className?: string;
 }) => (
   <div
-    className={`absolute left-0 top-0 flex h-full w-1/4 flex-col gap-0.5 rounded-lg bg-gray-200 shadow-md ${className}`}
+    className={`absolute left-0 top-0 flex h-full w-1/4 flex-col gap-0.5 bg-gray-200 p-2 shadow-md ${className}`}
   >
     {children}
   </div>
