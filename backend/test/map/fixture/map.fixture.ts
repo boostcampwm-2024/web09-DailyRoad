c617348f235085beac1f9dbb0de59fb8566648e1
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-15 21:01:12 +0900

    test: map 모듈의 repository 테스트 작성 #54

---
+++ backend/test/map/fixture/map.fixture.ts
@@ -0,0 +1,14 @@
+import { MapFixtureType } from './map.fixture.type';
+import { Map } from '../../../src/map/entity/map.entity';
+
+export class MapFixture {
+  static createMap = ({
+    user,
+    title = 'Map For Test',
+    isPublic = true,
+    thumbnailUrl = 'https://example.com/map_for_test.jpg',
+    description = 'A sample map with popular places',
+  }: MapFixtureType) => {
+    return new Map(user, title, isPublic, thumbnailUrl, description);
+  };
+}
---
+++ backend/test/map/fixture/map.fixture.type.ts
@@ -0,0 +1,9 @@
+import { User } from '../../../src/user/entity/user.entity';
+
+export type MapFixtureType = {
+  user: User;
+  title?: string;
+  isPublic?: boolean;
+  thumbnailUrl?: string;
+  description?: string;
+};
