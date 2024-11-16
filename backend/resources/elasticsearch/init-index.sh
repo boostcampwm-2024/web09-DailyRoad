7c9e52d52287901de6420243d11127bb1dc0d29a
koomchang <leahpar0401@gmail.com>
2024-11-17 02:13:29 +0900

    config: 로컬에서 elasticsearch와 kibana를 인증 없이 사용할 수 있도록 설정 #138

---
+++ backend/resources/elasticsearch/init-index.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+until curl -s -XGET "http://localhost:9200/_cluster/health" | grep '"status":"green"' > /dev/null; do
+  echo "Waiting for Elasticsearch to be ready..."
+  sleep 5
+done
+
+# Place 인덱스 확인 및 생성
+echo "Checking if 'place' index exists..."
+response=$(curl -s -o /dev/null -w "%{http_code}" -XGET "http://localhost:9200/place")
+
+if [ "$response" -eq 404 ]; then
+  echo "Index 'place' does not exist. Creating it..."
+  curl -X PUT "http://localhost:9200/place" \
+       -H "Content-Type: application/json" \
+       --data-binary @/usr/share/elasticsearch/place-index.json
+  echo "'place' index created successfully."
+elif [ "$response" -eq 200 ]; then
+  echo "Index 'place' already exists."
+else
+  echo "Unexpected response while checking for 'place' index: HTTP $response"
+fi
