8875fa951b5d2be1067aa550f7b7cb3bfeaefbce
1119wj <1119wj@naver.com>
2024-11-18 08:55:09 +0900

    feat: 라우팅 및 서스펜스 #101

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,36 @@ import MapCreateCoursePage from '@/pages/MapCreation/MapCreateCoursePage';
 import PlaceCreatePage from '@/pages/PlaceCreation/PlaceCreatePage';
 import LayoutCreate from '@/LayoutCreate';
 import NotFound from './pages/NotFound';
+import MapDetailPage from './pages/MapDetail/MapDetailPage';
+import { Suspense } from 'react';
+import MapEditPage from './pages/MapEditPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/map" element={<LayoutCreate />}></Route>
+
+        <Route
+          path="/map"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <LayoutCreate />
+            </Suspense>
+          }
+        >
+          <Route path=":id" element={<MapDetailPage />} />
+        </Route>
 
         <Route path="/create" element={<LayoutCreate />}>
           <Route index element={<MapPage />} />
           <Route path="map/:id" element={<MapCreateMapPage />} />
+          <Route path="course/:id" element={<MapCreateCoursePage />} />
+          <Route path="map/:id/:placeId" element={<PlaceCreatePage />} />
+          <Route path="course/:id/:placeId" element={<PlaceCreatePage />} />
+        </Route>
+        <Route path="/edit" element={<LayoutCreate />}>
+          <Route path="map/:id" element={<MapEditPage />} />
           <Route path="course/:id" element={<MapCreateCoursePage />} />
           <Route path="map/:id/:placeId" element={<PlaceCreatePage />} />
           <Route path="course/:id/:placeId" element={<PlaceCreatePage />} />
