4fed4cce5416ae6a6ff0569a85874432853fde5f
1119wj <1119wj@naver.com>
2024-11-18 08:54:43 +0900

    feat: 지도 수정 api 구현 #101

--- frontend/src/api/map/index.ts
+++ frontend/src/api/map/index.ts
@@ -5,6 +5,15 @@ import { END_POINTS } from '@/constants/api';
 type MapResponse = {
   id: number;
 };
+type EditInfoResponse = {
+  id: number;
+  title: string;
+  description: string;
+};
+type EditVisResponse = {
+  id: number;
+  isPublic: boolean;
+};
 
 export const getMap = async (mapId: number) => {
   const { data } = await axiosInstance.get<Map>(END_POINTS.MAP(mapId));
@@ -35,5 +44,57 @@ export const getMyMapList = async () => {
 
 export const getCourseList = async () => {
   const { data } = await axiosInstance.get<MapList>(END_POINTS.COURSES);
+  return data;
+};
+
+export const editMapInfo = async ({
+  title,
+  description,
+  mapId,
+}: {
+  title: string;
+  description: string;
+  mapId: number;
+}) => {
+  const { data } = await axiosInstance.patch<EditInfoResponse>(
+    END_POINTS.EDIT_MAP_INFO(mapId),
+    {
+      title,
+      description,
+    },
+  );
+  return data;
+};
+
+export const editMapVisibility = async ({
+  mapId,
+  isPublic,
+}: {
+  mapId: number;
+  isPublic: boolean;
+}) => {
+  const { data } = await axiosInstance.patch<EditVisResponse>(
+    END_POINTS.EDIT_MAP_VISIBILITY(mapId),
+    {
+      isPublic,
+    },
+  );
+  return data;
+};
+
+export const editMap = async (data: BaseMap & { mapId: number }) => {
+  const [infoResponse, visibilityResponse] = await Promise.all([
+    editMapInfo({
+      title: data.title,
+      description: data.description,
+      mapId: data.mapId,
+    }),
+    editMapVisibility({ mapId: data.mapId, isPublic: data.isPublic }),
+  ]);
+  return { ...infoResponse, ...visibilityResponse };
+};
+
+export const deleteMap = async (mapId: number) => {
+  const { data } = await axiosInstance.delete(END_POINTS.MAP(mapId));
   return data;
 };
