75a6641a91598e01e60c31e180db8c2ff30916f3
1119wj <1119wj@naver.com>
2024-11-18 18:34:07 +0900

    feat: 장소 추가 toast 구현

--- frontend/src/components/Place/DetailPlaceForm.tsx
+++ frontend/src/components/Place/DetailPlaceForm.tsx
@@ -9,18 +9,19 @@ import DashBoardHeader from '../common/DashBoardHeader';
 import { CreateMapType, CustomPlace, MarkerColor } from '@/types';
 import ColorSelector from '@/pages/PlaceCreation/ColorSelector';
 import { useAddPlaceMutation } from '@/hooks/api/useAddPlaceMutation';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 const DetailPlaceForm = () => {
   const place = useStore((state) => state.place);
+  const addToast = useStore((state) => state.addToast);
 
   const [description, setDescription] = useState('');
-
   const [activeColor, setActiveColor] = useState<MarkerColor | null>(null);
+
   const location = useLocation();
-  const navigate = useNavigate();
   const mode = location.pathname.split('/')[2].toUpperCase() as CreateMapType;
   const id = Number(useParams().id);
+
   const addPlaceMutation = useAddPlaceMutation(mode);
 
   const placeMarker: CustomPlace = {
@@ -34,53 +35,55 @@ const DetailPlaceForm = () => {
       { id, ...placeMarker },
       {
         onSuccess: () => {
-          navigate(-1);
+          addToast('장소가 추가되었습니다.', '', 'success');
         },
       },
     );
   };
 
   return (
-    <BaseWrapper>
-      <Box role="region" aria-label="장소 상세 정보">
-        <header className="flex gap-2">
-          <DashBoardHeader title="장소 추가하기" />
-        </header>
-        <PlaceItem place={place} isDetailPage={true} />
-      </Box>
-      <Box>
-        <h2 className="text-xl font-semibold">한줄평</h2>
-        <TextInputArea
-          value={description}
-          onChange={(prev) => setDescription(prev)}
-          placeholder={'한줄평을 입력해주세요.'}
-          maxLength={150}
-          height={160}
-          isTextArea={true}
-          aria-labelledby="review-heading"
+    place.id && (
+      <BaseWrapper position="" top="" left="" className="w-1/2">
+        <Box role="region" aria-label="장소 상세 정보">
+          <header className="flex gap-2">
+            <DashBoardHeader title="장소 추가하기" />
+          </header>
+          <PlaceItem place={place} isDetailPage={true} />
+        </Box>
+        <Box>
+          <h2 className="text-xl font-semibold">한줄평</h2>
+          <TextInputArea
+            value={description}
+            onChange={(prev) => setDescription(prev)}
+            placeholder={'한줄평을 입력해주세요.'}
+            maxLength={150}
+            height={160}
+            isTextArea={true}
+            aria-labelledby="review-heading"
+          />
+        </Box>
+        <ColorSelector
+          activeColor={activeColor}
+          setActiveColor={setActiveColor}
         />
-      </Box>
-      <ColorSelector
-        activeColor={activeColor}
-        setActiveColor={setActiveColor}
-      />
-      <Box>
-        <button
-          onClick={onSubmit}
-          className={`h-14 w-full rounded-md ${activeColor ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
-          disabled={!activeColor && !place && !description}
-        >
-          완료
-        </button>
-      </Box>
-      <Marker
-        key={place.google_place_id}
-        position={{
-          lat: place.location.latitude,
-          lng: place.location.longitude,
-        }}
-      />
-    </BaseWrapper>
+        <Box>
+          <button
+            onClick={onSubmit}
+            className={`h-14 w-full rounded-md ${activeColor ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
+            disabled={!activeColor && !place && !description}
+          >
+            완료
+          </button>
+        </Box>
+        <Marker
+          key={place.google_place_id}
+          position={{
+            lat: place.location.latitude,
+            lng: place.location.longitude,
+          }}
+        />
+      </BaseWrapper>
+    )
   );
 };
 
