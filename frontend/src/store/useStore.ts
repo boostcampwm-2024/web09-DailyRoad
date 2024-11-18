6d91b53d87fada0bd16a22ba9fff3638d1bbf7a0
1119wj <1119wj@naver.com>
2024-11-18 18:31:08 +0900

    feat: toast slice 추가

--- frontend/src/store/useStore.ts
+++ frontend/src/store/useStore.ts
@@ -2,8 +2,10 @@ import { create } from 'zustand';
 import { StoreState } from '@/types';
 import { createGoogleMapSlice } from '@/store/googleMapSlice';
 import { createPlaceSlice } from '@/store/placeSlice';
+import { createToastSlice } from './toastSlice';
 
 export const useStore = create<StoreState>()((...a) => ({
   ...createGoogleMapSlice(...a),
   ...createPlaceSlice(...a),
+  ...createToastSlice(...a),
 }));
