737e154c695492f2508d8338250f73632c103095
1119wj <1119wj@naver.com>
2024-11-18 18:34:29 +0900

    feat: 장소 추가 form 패널로 변경

--- frontend/src/components/Place/SearchPanel.tsx
+++ frontend/src/components/Place/SearchPanel.tsx
@@ -7,21 +7,27 @@ import DashBoardHeader from '@/components/common/DashBoardHeader';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import AddPlaceButton from './AddPlaceButton';
+import SideContainer from '../common/SideContainer';
+import DetailPlaceForm from './DetailPlaceForm';
 
 const SearchPanel = () => {
   const [query, setQuery] = useState('');
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(true);
 
   return (
-    <BaseWrapper>
-      <Box>
-        <DashBoardHeader title="장소 검색" />
-        <SearchBar onSearch={(newQuery) => setQuery(newQuery)} />
-      </Box>
-      <Box>
-        <SearchResults query={query} />
-      </Box>
-      <AddPlaceButton />
-    </BaseWrapper>
+    <SideContainer>
+      <BaseWrapper position="" top="" left="" className="w-1/2">
+        <Box>
+          <DashBoardHeader title="장소 검색" />
+          <SearchBar onSearch={(newQuery) => setQuery(newQuery)} />
+        </Box>
+        <Box>
+          <SearchResults query={query} />
+        </Box>
+        <AddPlaceButton />
+      </BaseWrapper>
+      {isSidePanelOpen && <DetailPlaceForm />}
+    </SideContainer>
   );
 };
 
