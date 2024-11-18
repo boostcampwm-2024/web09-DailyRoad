517156cf945528d8025d5cd34e34b2182d02294a
koomchang <leahpar0401@gmail.com>
2024-11-18 17:18:06 +0900

    feat: es가 적용 된 장소 검색 api #138

--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from '@src/search/search.service';
+import { ParseOptionalNumberPipe } from '@src/common/pipe/ParseOptionalNumberPipe';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
   @Get()
-  async search(@Query('query') query: string) {
-    return await this.searchService.search(query);
+  async search(
+    @Query('query') query: string,
+    @Query('lat') lat?: number,
+    @Query('lon') lon?: number,
+    @Query('page', new ParseOptionalNumberPipe(1)) page?: number,
+    @Query('limit', new ParseOptionalNumberPipe(10)) limit?: number,
+  ) {
+    return await this.searchService.search(query, lat, lon, page, limit);
   }
 
   @Get('index/place')
