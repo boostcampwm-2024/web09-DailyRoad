6df9f5f133a314e7eea359f837b47d6447b21571
koomchang <leahpar0401@gmail.com>
2024-11-19 13:12:11 +0900

    fix: es kibana 비밀번호 설정 경로 오류 수정 #138

--- backend/resources/elasticsearch/init-index.sh
+++ backend/resources/elasticsearch/init-index.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-until curl -s -XGET "http://localhost:9200/_cluster/health" | grep '"status":"green"' > /dev/null; do
+# Elasticsearch 준비 상태 확인
+until curl -s -XGET "http://localhost:9200/_cluster/health" -u "elastic:${ELASTIC_PASSWORD}" | grep '"status":"green"' > /dev/null; do
   echo "Waiting for Elasticsearch to be ready..."
   sleep 5
 done
 
-# Place 인덱스 확인 및 생성
+# Kibana 시스템 계정 비밀번호 설정
+echo "Setting kibana_system password..."
+curl -s -X POST -u "elastic:${ELASTIC_PASSWORD}" \
+     -H "Content-Type: application/json" \
+     -d "{\"password\":\"${ELASTIC_PASSWORD}\"}" \
+     "http://localhost:9200/_security/user/kibana_system/_password"
+
+# "place" 인덱스 확인 및 생성
 echo "Checking if 'place' index exists..."
-response=$(curl -s -o /dev/null -w "%{http_code}" -XGET "http://localhost:9200/place")
+response=$(curl -s -o /dev/null -w "%{http_code}" -XGET "http://localhost:9200/place" -u "elastic:${ELASTIC_PASSWORD}")
 
 if [ "$response" -eq 404 ]; then
-  echo "Setting kibana_system password";
-  until curl -s -X POST -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" http://elastic:9200/_security/user/kibana_system/_password -d "{\"password\":\"${ELASTIC_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
-  echo "All done!";
   echo "Index 'place' does not exist. Creating it..."
   curl -X PUT "http://localhost:9200/place" \
+       -u "elastic:${ELASTIC_PASSWORD}" \
        -H "Content-Type: application/json" \
        --data-binary @/usr/share/elasticsearch/place-index.json
   echo "'place' index created successfully."
