a15c68f5f9fe3fe41dd3bb7261372f9e93068994
1119wj <1119wj@naver.com>
2024-11-21 01:13:49 +0900

    style: 사소한 수정 #144

--- frontend/src/components/Place/AddPlaceButton.tsx
+++ frontend/src/components/Place/AddPlaceButton.tsx
@@ -1,17 +1,18 @@
 import { useStore } from '@/store/useStore';
 import Box from '../common/Box';
-import { useLocation, useNavigate } from 'react-router-dom';
 
-const AddPlaceButton = () => {
+type AddPlaceButtonProps = {
+  onClick: () => void;
+};
+
+const AddPlaceButton = ({ onClick }: AddPlaceButtonProps) => {
   const activePlace = useStore((state) => state.place);
-  const navigate = useNavigate();
-  const location = useLocation();
   return (
     <Box>
       <button
         className={`h-14 w-full rounded-md ${activePlace ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
         disabled={!activePlace}
-        onClick={() => navigate(`${location.pathname}/${activePlace?.id}`)}
+        onClick={onClick}
       >
         장소 추가하기
       </button>
