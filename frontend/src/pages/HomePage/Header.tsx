c07302338a138d2ec6b72668904db666b8c692c2
1119wj <1119wj@naver.com>
2024-11-21 01:02:36 +0900

    feat: 로그인 redirect 페이지 #144

--- frontend/src/pages/HomePage/Header.tsx
+++ frontend/src/pages/HomePage/Header.tsx
@@ -1,9 +1,17 @@
+import { useStore } from '@/store/useStore';
+
 const Header = () => {
+  const user = useStore((state) => state.user);
+  const handleLogin = () => {
+    window.location.href = import.meta.env.VITE_GOOGLE_AUTH_URL;
+  };
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
         <h1 className="text-2xl font-bold">Logo</h1>
-        <button className="">로그인</button>
+        <button className="" onClick={handleLogin}>
+          {user ? user.nickname : '로그인'}
+        </button>
       </div>
     </header>
   );
---
+++ frontend/src/pages/RedirectPage.tsx
@@ -0,0 +1,24 @@
+import { useLogInMutation } from '@/hooks/api/useLoginMutation';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const RedirectPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const code = searchParams.get('code');
+
+  if (!code) {
+    throw new Error('로그인/회원가입에 실패했습니다.');
+  }
+
+  const mutateLogIn = useLogInMutation();
+
+  useEffect(() => {
+    console.log('code', code);
+    mutateLogIn.mutate(code);
+  }, []);
+
+  return <div>로그인 중입니다.</div>;
+};
+
+export default RedirectPage;
