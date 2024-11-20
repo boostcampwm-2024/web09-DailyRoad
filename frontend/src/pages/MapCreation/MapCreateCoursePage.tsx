94d09fba02d3616c322829d7524472855e818ab4
1119wj <1119wj@naver.com>
2024-11-21 00:51:27 +0900

    feat: 지도/코스 생성페이지 쿼리 로직 추가 #144

--- frontend/src/pages/MapCreation/MapCreateCoursePage.tsx
+++ frontend/src/pages/MapCreation/MapCreateCoursePage.tsx
@@ -1,5 +1,12 @@
+import SearchPanel from '@/components/Place/SearchPanel';
+import { useCourseQuery } from '@/hooks/api/useCourseQuery';
+import { useParams } from 'react-router-dom';
+
 const MapCreateCoursePage = () => {
-  return <div>MapCreateCoursePage</div>;
+  const { id } = useParams();
+  const mapData = useCourseQuery(Number(id));
+
+  return <SearchPanel mapData={mapData} />;
 };
 
 export default MapCreateCoursePage;
