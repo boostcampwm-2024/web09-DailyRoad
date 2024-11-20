afebaea27af397661636b91c346c7af2315ab870
koomchang <leahpar0401@gmail.com>
2024-11-20 20:32:22 +0900

    refactor: es 에서 사용하는 filter 쿼리 분리 #138

---
+++ backend/src/search/query/builder/FilterQueryBuilder.ts
@@ -0,0 +1,26 @@
+const createTermFilter = (field: string, value: string, weight: number) => ({
+  filter: {
+    term: {
+      [field]: value,
+    },
+  },
+  weight,
+});
+
+const createMatchFilter = (field: string, value: string, weight: number) => ({
+  filter: {
+    match: {
+      [field]: value,
+    },
+  },
+  weight,
+});
+
+export const FilterBuilders = {
+  FILTER_TERM_NAME_KEYWORD: (query: string, weight: number = 20) =>
+    createTermFilter('name.keyword', query, weight),
+  FILTER_MATCH_NAME: (token: string, weight: number = 15) =>
+    createMatchFilter('name', token, weight),
+  FILTER_MATCH_FORMATTED_ADDRESS: (token: string, weight: number = 10) =>
+    createMatchFilter('formattedAddress', token, weight),
+};
