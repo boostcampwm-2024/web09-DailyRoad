4ce91727be5aab9b56476e0638a481c950fe3fb5
1119wj <1119wj@naver.com>
2024-11-21 01:09:06 +0900

    feat: mode에 따른 mutateFn 분기 #144

--- frontend/src/hooks/api/useDeleteMapMutation.ts
+++ frontend/src/hooks/api/useDeleteMapMutation.ts
@@ -1,12 +1,18 @@
+import { deleteCourse } from '@/api/course';
 import { deleteMap } from '@/api/map';
+import { CreateMapType } from '@/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-const useDeleteMapMutation = () => {
+const useDeleteMapMutation = (mode: CreateMapType) => {
   const queryClient = useQueryClient();
+
+  const mutationFn = mode === 'MAP' ? deleteMap : deleteCourse;
+  const queryKey = mode === 'MAP' ? 'maps' : 'courses';
+
   const deleteMutation = useMutation({
-    mutationFn: deleteMap,
+    mutationFn: mutationFn,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['maps'] });
+      queryClient.invalidateQueries({ queryKey: [queryKey] });
     },
   });
   return deleteMutation;
--- frontend/src/hooks/api/useDeletePlaceMutation.ts
+++ frontend/src/hooks/api/useDeletePlaceMutation.ts
@@ -1,13 +1,18 @@
-import { deletePlaceToMap } from '@/api/place';
+import { deletePlaceToCourse, deletePlaceToMap } from '@/api/place';
+import { CreateMapType } from '@/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-const useDeletePlaceMutation = () => {
+const useDeletePlaceMutation = (mode: CreateMapType) => {
   const queryClient = useQueryClient();
+
+  const mutationFn = mode === 'MAP' ? deletePlaceToMap : deletePlaceToCourse;
+  const queryKey = mode === 'MAP' ? 'map' : 'course';
+
   const deleteMutation = useMutation({
-    mutationFn: deletePlaceToMap,
+    mutationFn: mutationFn,
     onSuccess: (data) => {
       const id = data?.id;
-      queryClient.invalidateQueries({ queryKey: ['map', id] });
+      queryClient.invalidateQueries({ queryKey: [queryKey, id] });
     },
   });
   return deleteMutation;
