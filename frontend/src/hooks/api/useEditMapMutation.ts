bce2bd4d9162be9c59142d5b0deb8ff94c5692ac
1119wj <1119wj@naver.com>
2024-11-21 01:08:31 +0900

    fix: mutation 이름 변경, 쿼리키 변경 #144

--- frontend/src/hooks/api/useEditMapMutation.ts
+++ frontend/src/hooks/api/useEditMapMutation.ts
@@ -5,13 +5,14 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 export const useEditMapMutation = () => {
   const queryClient = useQueryClient();
 
-  const addMapMutation = useMutation({
+  const editMapMutation = useMutation({
     mutationFn: editMap,
     onSuccess: (data) => {
       const mapId = data?.id;
       queryClient.invalidateQueries({ queryKey: ['map', mapId] });
+      queryClient.invalidateQueries({ queryKey: ['maps'] });
     },
   });
 
-  return addMapMutation;
+  return editMapMutation;
 };
