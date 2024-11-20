949a5411119281437dc791996b4745ceced4f209
1119wj <1119wj@naver.com>
2024-11-21 00:49:43 +0900

    feat: 앱 시작 시 구글지도 api 로드 #144

---
+++ frontend/src/lib/googleMapsAPI-loader.ts
@@ -0,0 +1,66 @@
+import { Loader } from '@googlemaps/js-api-loader';
+
+let isApiLoaded = false;
+let GoogleMapClass: typeof google.maps.Map | null = null;
+let AdvancedMarkerElementClass:
+  | typeof google.maps.marker.AdvancedMarkerElement
+  | null = null;
+let PlaceClass: typeof google.maps.places.Place | null = null;
+
+export async function loadGoogleMapsApi() {
+  if (isApiLoaded) {
+    return;
+  }
+
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) {
+    throw new Error('Google Maps API 키가 설정되지 않았습니다.');
+  }
+
+  const loader = new Loader({
+    apiKey: apiKey,
+    version: '3.58',
+  });
+
+  await loader.load();
+
+  const { Map } = (await google.maps.importLibrary(
+    'maps',
+  )) as google.maps.MapsLibrary;
+
+  const { Marker } = (await google.maps.importLibrary(
+    'marker',
+  )) as google.maps.MarkerLibrary;
+  const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+    'marker',
+  )) as google.maps.MarkerLibrary;
+  const { Place } = (await google.maps.importLibrary(
+    'places',
+  )) as google.maps.PlacesLibrary;
+
+  GoogleMapClass = Map;
+  AdvancedMarkerElementClass = AdvancedMarkerElement;
+  PlaceClass = Place;
+  isApiLoaded = true;
+}
+
+export function getGoogleMapClass() {
+  if (!GoogleMapClass) {
+    throw new Error('Google Maps API가 아직 로드되지 않았습니다.');
+  }
+  return GoogleMapClass;
+}
+
+export function getMarkerClass() {
+  if (!AdvancedMarkerElementClass) {
+    throw new Error('Google Maps API가 아직 로드되지 않았습니다.');
+  }
+  return AdvancedMarkerElementClass;
+}
+
+export function getPlaceClass() {
+  if (!PlaceClass) {
+    throw new Error('Google Maps API가 아직 로드되지 않았습니다.');
+  }
+  return PlaceClass;
+}
