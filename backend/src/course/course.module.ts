12f5526ba89d554d7b1a3988ab68c74f718e0f67
Soap <miensop51@gmail.com>
2024-11-21 16:21:23 +0900

    feat: 코스 순서 저장 방식 개선 #90

--- backend/src/course/course.module.ts
+++ backend/src/course/course.module.ts
@@ -4,9 +4,11 @@ import { CourseController } from './course.controller';
 import { UserModule } from '../user/user.module';
 import { CourseRepository } from './course.repository';
 import { PlaceModule } from '../place/place.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoursePlace } from '@src/course/entity/course-place.entity';
 
 @Module({
-  imports: [UserModule, PlaceModule],
+  imports: [UserModule, PlaceModule, TypeOrmModule.forFeature([CoursePlace])],
   controllers: [CourseController],
   providers: [CourseService, CourseRepository],
 })
