700af4a35939b0071752b5795e4acebb0840a0be
1119wj <1119wj@naver.com>
2024-11-22 03:14:33 +0900

    style: 사소한 수정 #142
    #preview

--- frontend/src/pages/HomePage/Header.tsx
+++ frontend/src/pages/HomePage/Header.tsx
@@ -2,6 +2,7 @@ import { useStore } from '@/store/useStore';
 
 const Header = () => {
   const user = useStore((state) => state.user);
+  const isLogged = useStore((state) => state.isLogged);
   const handleLogin = () => {
     window.location.href = import.meta.env.VITE_GOOGLE_AUTH_URL;
   };
@@ -10,7 +11,7 @@ const Header = () => {
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
         <h1 className="text-2xl font-bold">Logo</h1>
         <button className="" onClick={handleLogin}>
-          {user ? user.nickname : '로그인'}
+          {isLogged ? user?.id : '로그인'}
         </button>
       </div>
     </header>
