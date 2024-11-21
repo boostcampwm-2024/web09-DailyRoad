8414213c0d3054bf1e8f0f92b01a8fa336456567
1119wj <1119wj@naver.com>
2024-11-22 03:11:18 +0900

    feat: 빈 객체 판별 로직 #142

--- frontend/src/components/Place/AddPlaceButton.tsx
+++ frontend/src/components/Place/AddPlaceButton.tsx
@@ -7,11 +7,12 @@ type AddPlaceButtonProps = {
 
 const AddPlaceButton = ({ onClick }: AddPlaceButtonProps) => {
   const activePlace = useStore((state) => state.place);
+
   return (
     <Box>
       <button
-        className={`h-14 w-full rounded-md ${activePlace ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
-        disabled={!activePlace}
+        className={`h-14 w-full rounded-md ${Boolean(activePlace.id) ? 'bg-c_bg_blue' : 'bg-c_button_gray'} p-4 text-xl font-semibold text-white`}
+        disabled={!Boolean(activePlace.id)}
         onClick={onClick}
       >
         장소 추가하기
