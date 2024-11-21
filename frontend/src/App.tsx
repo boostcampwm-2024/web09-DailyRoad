21381288fbc35997f3d04c9a4f7f6de5eb02a38d
1119wj <1119wj@naver.com>
2024-11-22 00:58:21 +0900

    style: 사용하지 않는 페이지 제거 #142

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Homepage from '@/pages/HomePage/HomePage';
 import MapPage from '@/pages/MapCreation/MapPage';
 import MapCreateMapPage from '@/pages/MapCreation/MapCreateMapPage';
 import MapCreateCoursePage from '@/pages/MapCreation/MapCreateCoursePage';
-import PlaceCreatePage from '@/pages/PlaceCreation/PlaceCreatePage';
 import LayoutCreate from '@/LayoutCreate';
 import NotFound from './pages/NotFound';
 import MapDetailPage from './pages/MapDetail/MapDetailPage';
@@ -36,16 +35,13 @@ function App() {
           <Route path="/create" element={<LayoutCreate />}>
             <Route index element={<MapPage />} />
             <Route path="map/:id" element={<MapCreateMapPage />} />
-            <Route path="map/:id/:placeId" element={<PlaceCreatePage />} />
+
             <Route path="course/:id" element={<MapCreateCoursePage />} />
-            <Route path="course/:id/:placeId" element={<PlaceCreatePage />} />
           </Route>
 
           <Route path="/edit" element={<LayoutCreate />}>
             <Route path="map/:id" element={<MapEditPage />} />
             <Route path="course/:id" element={<MapCreateCoursePage />} />
-            <Route path="map/:id/:placeId" element={<PlaceCreatePage />} />
-            <Route path="course/:id/:placeId" element={<PlaceCreatePage />} />
           </Route>
 
           <Route path="/auth/callback" element={<RedirectPage />} />
--- frontend/src/pages/PlaceCreation/PlaceCreatePage.tsx
+++
@@ -1,7 +0,0 @@
-import DetailPlaceForm from '@/components/Place/DetailPlaceForm';
-
-const PlaceCreatePage = () => {
-  return <DetailPlaceForm />;
-};
-
-export default PlaceCreatePage;
