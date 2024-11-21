06dee97a9344918a63ce4da135df4e20d85bffad
1119wj <1119wj@naver.com>
2024-11-22 03:09:12 +0900

    fix: 페이지 언마운트 시 전역상태 초기화 #142

--- frontend/src/pages/MapCreation/MapCreateMapPage.tsx
+++ frontend/src/pages/MapCreation/MapCreateMapPage.tsx
@@ -1,10 +1,18 @@
 import SearchPanel from '@/components/Place/SearchPanel';
 import { useMapQuery } from '@/hooks/api/useMapQuery';
+import { useStore } from '@/store/useStore';
+import { Place } from '@/types';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const MapCreateMapPage = () => {
   const { id } = useParams();
   const mapData = useMapQuery(Number(id));
+  const setPlace = useStore((state) => state.setPlace);
+
+  useEffect(() => {
+    setPlace({} as Place);
+  }, []);
 
   return <SearchPanel mapData={mapData} />;
 };
