86118f80b2bd49eab773fae108e31852c2280c4c
1119wj <1119wj@naver.com>
2024-11-22 01:04:59 +0900

    feat: 기본 테스트 토큰 추가 #142
    #preview

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,9 +3,14 @@ import axios from 'axios';
 import { AXIOS_BASE_URL, NETWORK } from '@/constants/api';
 import { checkAndSetToken, handleAPIError } from './interceptors';
 
+const accessToken = import.meta.env.VITE_TEST_ACCESS_TOKEN as string;
+
 export const axiosInstance = axios.create({
   baseURL: AXIOS_BASE_URL,
   timeout: NETWORK.TIMEOUT,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
   withCredentials: true,
   useAuth: true,
 });
