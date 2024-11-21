f83a51539aa8a38f2b5dff4404cd4b0db375bf4d
1119wj <1119wj@naver.com>
2024-11-22 03:10:19 +0900

    feat: 지도 삭제 시 토스트, 및 navigate #142

--- frontend/src/components/Map/DeleteMapButton.tsx
+++ frontend/src/components/Map/DeleteMapButton.tsx
@@ -1,5 +1,7 @@
 import useDeleteMapMutation from '@/hooks/api/useDeleteMapMutation';
+import { useStore } from '@/store/useStore';
 import { CreateMapType } from '@/types';
+import { useNavigate } from 'react-router-dom';
 
 /**todo : 삭제 모달 구현*/
 
@@ -9,10 +11,18 @@ type DeleteMapButtonProps = {
 };
 
 const DeleteMapButton = ({ mapId, text }: DeleteMapButtonProps) => {
-  const mode = location.pathname.split('/')[2].toUpperCase() as CreateMapType;
+  const mode = location.pathname.split('/')[1].toUpperCase() as CreateMapType;
   const deleteMutation = useDeleteMapMutation(mode);
+  const addToast = useStore((state) => state.addToast);
+  const navigate = useNavigate();
+
   const onClick = () => {
-    deleteMutation.mutate(mapId);
+    deleteMutation.mutate(mapId, {
+      onSuccess: () => {
+        addToast('지도가 삭제되었습니다.', '', 'success');
+        navigate('/');
+      },
+    });
   };
   return (
     <button onClick={onClick} className="text-xs text-c_placeholder_gray">
