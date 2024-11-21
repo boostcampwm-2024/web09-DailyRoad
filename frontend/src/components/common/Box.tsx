7df10a9a194d4c99152b9c825ce54f060a80d11e
1119wj <1119wj@naver.com>
2024-11-22 00:42:24 +0900

    feat: dnd를 위한 ref #142

--- frontend/src/components/common/Box.tsx
+++ frontend/src/components/common/Box.tsx
@@ -1,18 +1,26 @@
+import React from 'react';
+
 type BoxProps = {
   children: React.ReactNode;
   className?: string;
   role?: string;
 };
 
-const Box = ({ children, className = '', role = 'region' }: BoxProps) => {
-  return (
-    <div
-      role={role}
-      className={`w-full bg-white p-4 shadow-xl ${className} flex flex-col gap-2`}
-    >
-      {children}
-    </div>
-  );
-};
+const Box = React.forwardRef<HTMLDivElement, BoxProps>(
+  ({ children, className = '', role = 'region', ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        role={role}
+        {...props}
+        className={`w-full bg-white p-4 shadow-xl ${className} flex flex-col gap-2`}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+Box.displayName = 'Box';
 
 export default Box;
