21dbbbcaf5ad9f9a3f10963aabbd07a52ec30d32
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-11-23 16:03:34 +0900

    test: 지도 통합테스트 작성 #54

---
+++ backend/test/map/integration-test/map.integration.expectExcptions.ts
@@ -0,0 +1,28 @@
+export const INVALID_TOKEN_EXCEPTION = {
+  statusCode: 401,
+  message: '유효하지 않은 토큰입니다.',
+};
+
+export const EXPIRE_TOKEN_EXCEPTION = {
+  statusCode: 401,
+  message: '만료된 토큰입니다.',
+};
+
+export const EMPTY_TOKEN_EXCEPTION = {
+  statusCode: 401,
+  message: '토큰이 없습니다.',
+};
+
+export const MAP_NOT_FOUND_EXCEPTION = (id: number) => {
+  return {
+    statusCode: 404,
+    message: `id:${id} 지도가 존재하지 않거나 삭제되었습니다.`,
+  };
+};
+
+export const MAP_PERMISSION_EXCEPTION = (id: number) => {
+  return {
+    statusCode: 403,
+    message: `지도 ${id} 에 대한 권한이 없습니다.`,
+  };
+};
