114a45c0453d1f258c0d924fbe95da2327b25024
koomchang <leahpar0401@gmail.com>
2024-11-23 18:41:05 +0900

    feat: 코스 사용자 확인 권한에서 admin은 모두 통과할 수 있도록 설정 #163

--- backend/src/course/course.module.ts
+++ backend/src/course/course.module.ts
@@ -6,10 +6,24 @@ import { CourseRepository } from './course.repository';
 import { PlaceModule } from '../place/place.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursePlace } from '@src/course/entity/course-place.entity';
+import { AdminGuard } from '@src/admin/guard/AdminGuard';
+import { CoursePermissionGuard } from '@src/course/guards/CoursePermissionGuard';
+import { AdminModule } from '@src/admin/admin.module';
 
 @Module({
-  imports: [UserModule, PlaceModule, TypeOrmModule.forFeature([CoursePlace])],
+  imports: [
+    UserModule,
+    PlaceModule,
+    AdminModule,
+    TypeOrmModule.forFeature([CoursePlace]),
+  ],
   controllers: [CourseController],
-  providers: [CourseService, CourseRepository],
+  providers: [
+    CourseService,
+    CourseRepository,
+    CoursePermissionGuard,
+    AdminGuard,
+  ],
+  exports: [CoursePermissionGuard],
 })
 export class CourseModule {}
