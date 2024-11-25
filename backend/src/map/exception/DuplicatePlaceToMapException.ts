6bd7573ed5d34f61875b3da95270c3fd6846b1fe
Soap <miensop51@gmail.com>
2024-11-26 01:51:47 +0900

    refactor: 지도 관련 예외 점검 및 코드 수정 #187

--- backend/src/map/exception/DuplicatePlaceToMapException.ts
+++ backend/src/map/exception/DuplicatePlaceToMapException.ts
@@ -1,10 +1,10 @@
-import { BaseException } from '../../common/exception/BaseException';
+import { BaseException } from '@src/common/exception/BaseException';
 import { HttpStatus } from '@nestjs/common';
 
 export class DuplicatePlaceToMapException extends BaseException {
   constructor(id: number) {
     super({
-      code: 805,
+      code: 303,
       message: '이미 지도에 존재하는 장소입니다. : ' + id,
       status: HttpStatus.CONFLICT,
     });
