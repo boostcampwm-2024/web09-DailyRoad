920628c00850479f4277db32b3393c410a30ffce
Soap <miensop51@gmail.com>
2024-11-26 01:51:35 +0900

    refactor: 장소 관련 예외 점검 및 코드 수정 #187

--- backend/src/place/exception/PlaceAlreadyExistsException.ts
+++ backend/src/place/exception/PlaceAlreadyExistsException.ts
@@ -1,10 +1,10 @@
-import { BaseException } from '../../common/exception/BaseException';
+import { BaseException } from '@src/common/exception/BaseException';
 import { HttpStatus } from '@nestjs/common';
 
 export class PlaceAlreadyExistsException extends BaseException {
   constructor() {
     super({
-      code: 1001,
+      code: 203,
       message: '장소가 이미 존재합니다.',
       status: HttpStatus.CONFLICT,
     });
