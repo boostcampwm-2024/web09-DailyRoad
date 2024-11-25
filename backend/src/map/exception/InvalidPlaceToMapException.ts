88cdace264497077720008dc7696e49ab2000e3e
Soap <miensop51@gmail.com>
2024-11-26 01:51:47 +0900

    refactor: 지도 관련 예외 점검 및 코드 수정 #187

--- backend/src/map/exception/InvalidPlaceToMapException.ts
+++ backend/src/map/exception/InvalidPlaceToMapException.ts
@@ -1,10 +1,10 @@
-import { BaseException } from '../../common/exception/BaseException';
+import { BaseException } from '@src/common/exception/BaseException';
 import { HttpStatus } from '@nestjs/common';
 
 export class InvalidPlaceToMapException extends BaseException {
   constructor(invalidPlaceId: number) {
     super({
-      code: 803,
+      code: 304,
       message: `존재하지 않는 장소를 지도에 추가할 수 없습니다. : ${invalidPlaceId}`,
       status: HttpStatus.BAD_REQUEST,
     });
