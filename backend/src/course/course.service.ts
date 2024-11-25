a0d0813c049dffab6fe163d0554f36853b86a8ca
koomchang <leahpar0401@gmail.com>
2024-11-25 13:52:06 +0900

    fix: 코스에서 불필요한 transaction 제거 #170

--- backend/src/course/course.service.ts
+++ backend/src/course/course.service.ts
@@ -13,7 +13,6 @@ import { PlaceRepository } from '../place/place.repository';
 import { InvalidPlaceToCourseException } from './exception/InvalidPlaceToCourseException';
 import { PagedCourseResponse } from './dto/PagedCourseResponse';
 import { User } from '../user/entity/user.entity';
-import { Transactional } from 'typeorm-transactional';
 
 @Injectable()
 export class CourseService {
@@ -69,7 +68,6 @@ export class CourseService {
     return course.user.id;
   }
 
-  @Transactional()
   async createCourse(userId: number, createCourseForm: CreateCourseRequest) {
     const user = { id: userId } as User;
     const course = createCourseForm.toEntity(user);
@@ -77,7 +75,6 @@ export class CourseService {
     return { id: (await this.courseRepository.save(course)).id };
   }
 
-  @Transactional()
   async deleteCourse(id: number) {
     await this.validateCourseExistsById(id);
 
@@ -85,7 +82,6 @@ export class CourseService {
     return { id };
   }
 
-  @Transactional()
   async updateCourseInfo(
     id: number,
     updateCourseForm: UpdateCourseInfoRequest,
@@ -100,7 +96,6 @@ export class CourseService {
     );
   }
 
-  @Transactional()
   async updateCourseVisibility(id: number, isPublic: boolean) {
     await this.validateCourseExistsById(id);
     return this.courseRepository.updateIsPublicById(id, isPublic);
@@ -111,7 +106,6 @@ export class CourseService {
       throw new CourseNotFoundException(id);
   }
 
-  @Transactional()
   async setPlacesOfCourse(
     id: number,
     setPlacesOfCourseRequest: SetPlacesOfCourseRequest,
