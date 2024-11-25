d79761b9e1f03f793df6427e9cd217602064c093
Soap <miensop51@gmail.com>
2024-11-26 01:52:11 +0900

    refactor: 공통/유저/외부 서비스 관련 예외 점검 및 코드 수정 #187

--- backend/src/common/exception/EmptyRequestException.ts
+++ backend/src/common/exception/EmptyRequestException.ts
@@ -4,7 +4,7 @@ import { HttpStatus } from '@nestjs/common';
 export class EmptyRequestException extends BaseException {
   constructor(action: string = '작업') {
     super({
-      code: 4002,
+      code: 601,
       message: `${action}에 필요한 정보가 없습니다.`,
       status: HttpStatus.BAD_REQUEST,
     });
