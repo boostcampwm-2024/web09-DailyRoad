e8631673acce9dcf2a3485ce320d88db8804e4ab
1119wj <1119wj@naver.com>
2024-11-25 12:35:22 +0900

    feat: 썸네일 등록 및 조회 #124

--- frontend/src/components/Map/MapDetailBoard.tsx
+++ frontend/src/components/Map/MapDetailBoard.tsx
@@ -29,38 +29,49 @@ const MapDetailBoard = ({ mapData }: MapDetailBoardProps) => {
     <SideContainer>
       <BaseWrapper position="" top="" left="" className="w-1/2">
         <Box>
-          <div className="flex justify-between">
-            <DashBoardHeader title={title} />
-            <div className="flex items-center gap-1 text-center">
-              <EditMapButton text="수정" to={`/edit/map/${mapData.id}`} />
-              <p className="text-xs text-c_placeholder_gray">|</p>
-              <DeleteMapButton mapId={mapData.id} text="삭제" />
-            </div>
+          <DashBoardHeader title={title} />
+          <div className="flex items-center justify-end gap-1 text-center">
+            <EditMapButton text="수정" to={`/edit/map/${mapData.id}`} />
+            <p className="text-xs text-c_placeholder_gray">|</p>
+            <DeleteMapButton mapId={mapData.id} text="삭제" />
           </div>
-          <img src="/src/assets/Map.jpg" alt="map" className="h-40 w-full" />
+
+          {thumbnailUrl ? (
+            <img src={thumbnailUrl} alt="thumbnail" className="h-40 w-full" />
+          ) : (
+            <img src="/src/assets/Map.jpg" alt="map" className="h-40 w-full" />
+          )}
           <p className="text-lg font-semibold">지도 소개</p>
           <div className="rounded-md border-[1px] border-gray-100 p-1">
             {description}
           </div>
         </Box>
-        <Box className="scrollbar-thumb-rounded-lg overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-400 hover:scrollbar-track-gray-200 hover:scrollbar-thumb-gray-500">
+        <Box className="scrollbar-thumb-rounded-lg min-h-80 overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-400 hover:scrollbar-track-gray-200 hover:scrollbar-thumb-gray-500">
           <EditMapButton
             text="수정"
             to={`/create/map/${mapData.id}`}
             className="flex justify-end px-2"
           />
-          {places.map((place) => (
-            <div key={place.id} onClick={() => setIsSidePanelOpen(true)}>
-              <PlaceItem key={place.id} place={place} isDetailPage={true} />
-              <Marker
-                key={place.google_place_id}
-                position={{
-                  lat: place.location.latitude,
-                  lng: place.location.longitude,
-                }}
-              />
+          {places && places.length > 0 ? (
+            <>
+              {places.map((place) => (
+                <div key={place.id} onClick={() => setIsSidePanelOpen(true)}>
+                  <PlaceItem key={place.id} place={place} isDetailPage={true} />
+                  <Marker
+                    key={place.google_place_id}
+                    position={{
+                      lat: place.location.latitude,
+                      lng: place.location.longitude,
+                    }}
+                  />
+                </div>
+              ))}
+            </>
+          ) : (
+            <div className="flex h-full w-full items-center justify-center">
+              <p className="text-lg text-gray-400">등록된 장소가 없습니다.</p>
             </div>
-          ))}
+          )}
         </Box>
       </BaseWrapper>
       {isSidePanelOpen && customPlace && (
