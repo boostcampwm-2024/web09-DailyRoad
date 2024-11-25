283177a540a288f89c235dc4797d14dfef26daad
Soap <miensop51@gmail.com>
2024-11-26 01:51:22 +0900

    refactor: 코스 관련 예외 점검 및 코드 수정 #187

--- backend/src/course/exception/PlaceInCourseNotFoundException.ts
+++ backend/src/course/exception/PlaceInCourseNotFoundException.ts
@@ -4,7 +4,7 @@ import { HttpStatus } from '@nestjs/common';
 export class PlaceInCourseNotFoundException extends BaseException {
   constructor(mapId: number, mapPlaceId: number) {
     super({
-      code: 906,
+      code: 402,
       message: `[${mapId}] 코스에 [${mapPlaceId}] 장소가 존재하지 않거나 삭제되었습니다.`,
       status: HttpStatus.NOT_FOUND,
     });
