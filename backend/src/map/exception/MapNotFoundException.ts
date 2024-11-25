298fea5811e9341761fb8c72c8c5309378e5bb55
Soap <miensop51@gmail.com>
2024-11-26 01:51:47 +0900

    refactor: 지도 관련 예외 점검 및 코드 수정 #187

--- backend/src/map/exception/MapNotFoundException.ts
+++ backend/src/map/exception/MapNotFoundException.ts
@@ -1,10 +1,10 @@
-import { BaseException } from '../../common/exception/BaseException';
+import { BaseException } from '@src/common/exception/BaseException';
 import { HttpStatus } from '@nestjs/common';
 
 export class MapNotFoundException extends BaseException {
   constructor(id: number) {
     super({
-      code: 802,
+      code: 301,
       message: `id:${id} 지도가 존재하지 않거나 삭제되었습니다.`,
       status: HttpStatus.NOT_FOUND,
     });
