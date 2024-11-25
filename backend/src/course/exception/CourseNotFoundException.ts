2d0f68b885098599a2da039355141fcae7006322
Soap <miensop51@gmail.com>
2024-11-26 01:51:22 +0900

    refactor: 코스 관련 예외 점검 및 코드 수정 #187

--- backend/src/course/exception/CourseNotFoundException.ts
+++ backend/src/course/exception/CourseNotFoundException.ts
@@ -1,10 +1,10 @@
-import { BaseException } from '../../common/exception/BaseException';
+import { BaseException } from '@src/common/exception/BaseException';
 import { HttpStatus } from '@nestjs/common';
 
 export class CourseNotFoundException extends BaseException {
   constructor(id: number) {
     super({
-      code: 902,
+      code: 401,
       message: `id:${id} 코스가 존재하지 않거나 삭제되었습니다.`,
       status: HttpStatus.NOT_FOUND,
     });
--- backend/src/course/exception/InvalidPlaceToCourseException.ts
+++ backend/src/course/exception/InvalidPlaceToCourseException.ts
@@ -1,10 +1,10 @@
-import { BaseException } from '../../common/exception/BaseException';
+import { BaseException } from '@src/common/exception/BaseException';
 import { HttpStatus } from '@nestjs/common';
 
 export class InvalidPlaceToCourseException extends BaseException {
   constructor(invalidPlaceIds: number[]) {
     super({
-      code: 903,
+      code: 404,
       message: `존재하지 않는 장소를 코스에 추가할 수 없습니다. : ${invalidPlaceIds.join(', ')}`,
       status: HttpStatus.BAD_REQUEST,
     });
