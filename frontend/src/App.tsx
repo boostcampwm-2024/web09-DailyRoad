346e5b57036de814a95f359643ef8fe358ffd78e
1119wj <1119wj@naver.com>
2024-11-27 01:13:35 +0900

    feat: 라우트 선언 방식 변경 #124

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,67 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from '@/pages/HomePage/HomePage';
-import MapPage from '@/pages/MapCreation/MapPage';
-import MapCreateMapPage from '@/pages/MapCreation/MapCreateMapPage';
-import MapCreateCoursePage from '@/pages/MapCreation/MapCreateCoursePage';
-import LayoutCreate from '@/LayoutCreate';
-import NotFound from './pages/NotFound';
-import MapDetailPage from './pages/MapDetail/MapDetailPage';
-import { Suspense } from 'react';
-import MapEditPage from './pages/MapEditPage';
-import ToastContainer from './components/common/Toast/ToastContainer';
-import ErrorBoundary from './components/Error/ErrorBoundary';
-import ErrorFallback from './components/Error/ErrorFallback';
-import RedirectPage from './pages/RedirectPage';
-import CourseDetailPage from './pages/MapDetail/CourseDetailPage';
-import CourseEditPage from './pages/CourseEditPage';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 function App() {
-  return (
-    <ErrorBoundary Fallback={ErrorFallback}>
-      <Router>
-        <ToastContainer />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-
-          <Route
-            path="/map"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <LayoutCreate />
-              </Suspense>
-            }
-          >
-            <Route path=":id" element={<MapDetailPage />} />
-          </Route>
-          <Route
-            path="/course"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <LayoutCreate />
-              </Suspense>
-            }
-          >
-            <Route path=":id" element={<CourseDetailPage />} />
-          </Route>
-
-          <Route path="/create" element={<LayoutCreate />}>
-            <Route index element={<MapPage />} />
-            <Route path="map/:id" element={<MapCreateMapPage />} />
-
-            <Route path="course/:id" element={<MapCreateCoursePage />} />
-          </Route>
-
-          <Route path="/edit" element={<LayoutCreate />}>
-            <Route path="map/:id" element={<MapEditPage />} />
-            <Route path="course/:id" element={<CourseEditPage />} />
-          </Route>
-
-          <Route path="/auth/callback" element={<RedirectPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </ErrorBoundary>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
