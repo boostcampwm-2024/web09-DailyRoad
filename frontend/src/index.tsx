60160fce81d03f6a3892a7a04e3691c606e824fe
1119wj <1119wj@naver.com>
2024-11-27 01:14:11 +0900

    feat: lazy 적용, loading 컴포넌트 #124

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { StrictMode } from 'react';
-import { loadGoogleMapsApi } from './lib/googleMapsAPI-loader';
+import App from './App';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
