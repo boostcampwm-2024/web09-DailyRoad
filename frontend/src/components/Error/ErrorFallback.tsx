868f4244874224436eae0c17326d7d4b34e530c0
1119wj <1119wj@naver.com>
2024-11-27 01:17:24 +0900

    feat: 에러 시 메인 이동 #124

--- frontend/src/components/Error/ErrorFallback.tsx
+++ frontend/src/components/Error/ErrorFallback.tsx
@@ -1,3 +1,6 @@
+import { ROUTES } from '@/constants/routes';
+import { useNavigate } from 'react-router-dom';
+
 export type ErrorProps = {
   statusCode?: number;
   errorCode?: number;
@@ -5,12 +8,13 @@ export type ErrorProps = {
 };
 
 const ErrorFallback = ({ statusCode, errorCode, resetError }: ErrorProps) => {
+  const navigate = useNavigate();
   return (
     <div role="alert" style={{ padding: '20px', textAlign: 'center' }}>
       <h2>문제가 발생했습니다.</h2>
       {statusCode && <p>상태 코드: {statusCode}</p>}
       {errorCode && <p>에러 코드: {errorCode}</p>}
-      <button onClick={resetError}>다시 시도하기</button>
+      <button onClick={() => navigate(ROUTES.ROOT)}>메인으로 이동하기</button>
     </div>
   );
 };
