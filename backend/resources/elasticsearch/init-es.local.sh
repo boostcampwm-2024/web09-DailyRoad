0ecc84804153c53871ee15439aca92ea2dfab727
Soap <miensop51@gmail.com>
2024-11-27 17:50:00 +0900

    docs: ES 초기설정 스크립트 수정 #195

--- backend/resources/elasticsearch/init-es.local.sh
+++ backend/resources/elasticsearch/init-es.local.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+# 현재 스크립트의 디렉터리 가져오기
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+INDEX_FILE="$SCRIPT_DIR/index/place-index.json"
+
+# Elasticsearch 상태 확인
 until curl -s -XGET "http://localhost:9200/_cluster/health" | grep '"status":"green"' > /dev/null; do
   echo "Waiting for Elasticsearch to be ready..."
   sleep 5
 done
+
 # Place 인덱스 확인 및 생성
 echo "Checking if 'place' index exists..."
 response=$(curl -s -o /dev/null -w "%{http_code}" -XGET "http://localhost:9200/place")
@@ -10,7 +17,7 @@ if [ "$response" -eq 404 ]; then
   echo "Index 'place' does not exist. Creating it..."
   curl -X PUT "http://localhost:9200/place" \
        -H "Content-Type: application/json" \
-       --data-binary @/usr/share/elasticsearch/place-index.json
+       --data-binary @"$INDEX_FILE"
   echo "'place' index created successfully."
 elif [ "$response" -eq 200 ]; then
   echo "Index 'place' already exists."
