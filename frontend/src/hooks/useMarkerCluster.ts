de914826f61b74b3023d7315d7de6d2a11b8aa1b
1119wj <1119wj@naver.com>
2024-11-27 22:25:24 +0900

    feat: 마커 클러스터 로직 #124
    후에 분리 예정

--- frontend/src/hooks/useMarkerCluster.ts
+++
@@ -1,6 +0,0 @@
-import { useStore } from '@/store/useStore';
-
-export const useMarkerCluster = () => {
-  const map = useStore((state) => state.googleMap);
-  const markers = useStore((state) => state.markers);
-};
