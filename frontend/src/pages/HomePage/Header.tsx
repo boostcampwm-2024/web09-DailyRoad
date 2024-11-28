c4315846cba87ff52a82ee9f11df0c78cae5d2fc
1119wj <1119wj@naver.com>
2024-11-28 19:03:06 +0900

    fix: 로그인 로그아웃 로직 변경 #141

--- frontend/src/pages/HomePage/Header.tsx
+++ frontend/src/pages/HomePage/Header.tsx
@@ -1,52 +1,15 @@
-import { getRedirectUri } from '@/api/auth';
-import { useUserInfoQuery } from '@/hooks/api/useUserInfoQuery';
-import { useStore } from '@/store/useStore';
-import { useEffect } from 'react';
-import UserProfile from './UserProfile';
+import { Suspense } from 'react';
+
+import LoginButtons from './LoginButtons';
 
 const Header = () => {
-  const user = useStore((state) => state.user);
-  const isLogged = useStore((state) => state.isLogged);
-  const setUser = useStore((state) => state.setUser);
-  const data = useUserInfoQuery(localStorage.getItem('ACCESS_TOKEN_KEY'));
-  const handleLogin = async () => {
-    const redirectUri = await getRedirectUri();
-    window.location.href = redirectUri;
-  };
-  const handleAdminLogin = () => {
-    localStorage.setItem(
-      'ACCESS_TOKEN_KEY',
-      import.meta.env.VITE_TEST_ACCESS_TOKEN,
-    );
-    console.log(localStorage.getItem('ACCESS_TOKEN_KEY'));
-  };
-
-  useEffect(() => {
-    if (isLogged && data) {
-      setUser(data);
-      console.log('user', data);
-    }
-  }, [isLogged, data]);
-
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
         <h1 className="text-2xl font-bold">Logo</h1>
-        <div className="flex justify-between gap-1">
-          {!isLogged ? (
-            <button className="" onClick={handleLogin}>
-              구글 로그인
-            </button>
-          ) : user !== null ? (
-            <UserProfile user={user} />
-          ) : null}
-          <button
-            onClick={handleAdminLogin}
-            className="rounded-md border-2 border-gray-500 px-4 py-2"
-          >
-            테스트 id 로그인
-          </button>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginButtons />
+        </Suspense>
       </div>
     </header>
   );
---
+++ frontend/src/pages/HomePage/LogOutButton.tsx
@@ -0,0 +1,14 @@
+import { useLogOutMutation } from '@/hooks/api/useLogoutMutation';
+import { useStore } from '@/store/useStore';
+
+const LogOutButton = () => {
+  const logoutMutation = useLogOutMutation();
+  const logout = useStore((state) => state.logOut);
+  const onClick = () => {
+    logoutMutation.mutate();
+    logout();
+  };
+  return <button onClick={onClick}>LogOutButton</button>;
+};
+
+export default LogOutButton;
