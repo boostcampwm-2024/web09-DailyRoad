29886a29592f246e605a597dc47590a353584987
miensoap <miensop51@gmail.com>
2024-11-29 21:08:20 +0900

    feat: 메인 페이지에 검색창 컴포넌트 추가 #216

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import MainListPanel from './MainListPanel';
 
 import { useStore } from '@/store/useStore';
 import { useNavigate } from 'react-router-dom';
+import SearchBar from '@/components/common/SearchBar';
 
 const Homepage = () => {
   const addToast = useStore((state) => state.addToast);
@@ -20,15 +21,26 @@ const Homepage = () => {
   return (
     <>
       <Header />
-      <div className="flex justify-center p-4">
-        <button
-          onClick={onClick}
-          className="w-30 flex h-10 items-center justify-center rounded-md border-[1.5px] border-c_button_gray p-2 text-lg"
-        >
-          나만의 지도 만들러 가기
-        </button>
+      <div className={'flex w-full flex-col items-center justify-center'}>
+        <div className="mb-4 flex w-[1080px] flex-col items-center justify-center p-4">
+          <SearchBar onSearch={(keyword) => alert(keyword)}></SearchBar>
+
+          <div
+            className={
+              'm-10 flex h-[426px] w-full items-center justify-center bg-gray-300 font-extrabold'
+            }
+          >
+            배너/추천 등의 일러스트
+          </div>
+          <button
+            onClick={onClick}
+            className="border-c_brand_BLUE text-c_brand_BLUE flex h-10 h-[60px] w-[342px] items-center justify-center rounded-md border-[1.5px] p-2 text-lg"
+          >
+            나만의 지도 만들러 가기
+          </button>
+        </div>
+        <MainListPanel />
       </div>
-      <MainListPanel />
       <Footer />
     </>
   );
