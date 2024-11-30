cb9323cdf7de579f2f241e6ad3a34b87a831c7c9
miensoap <miensop51@gmail.com>
2024-11-30 14:59:37 +0900

    feat: 구글 API 키 필요한 장소 검색/등록 API 비활성화

--- backend/src/place/place.controller.ts
+++ backend/src/place/place.controller.ts
@@ -1,31 +1,25 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Query,
-  UseGuards,
-  BadRequestException,
-  Post,
-  Body,
-} from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, BadRequestException, Post, Body, Param } from '@nestjs/common';
 import { PlaceService } from './place.service';
 import { ParseOptionalNumberPipe } from '@src/common/pipe/ParseOptionalNumberPipe';
 import { JwtAuthGuard } from '@src/auth/JwtAuthGuard';
 import { Throttle } from '@nestjs/throttler';
 import { CreatePlaceRequest } from '@src/place/dto/CreatePlaceRequest';
+import { UnavailableIn } from '@src/common/decorator/UnavaliableIn';
 
 @Controller('places')
 export class PlaceController {
-  constructor(private readonly placeService: PlaceService) {}
+  constructor(private readonly placeService: PlaceService) {
+  }
 
   @Throttle({ default: { limit: 60, ttl: 60000 } })
   @UseGuards(JwtAuthGuard)
   @Post()
+  @UnavailableIn('lightweight')
   async importPlace(@Body() createPlaceDto: CreatePlaceRequest) {
+
     return this.placeService.addPlace(createPlaceDto);
   }
 
-  // Todo. 커스텀 스로틀러 구현
   @Throttle({
     default: {
       limit: 60,
@@ -36,6 +30,7 @@ export class PlaceController {
   })
   @UseGuards(JwtAuthGuard)
   @Get('/search')
+  @UnavailableIn('lightweight')
   async searchPlacesToImport(@Query('query') query: string) {
     if (!query) throw new BadRequestException('검색어를 입력해 주세요.');
 
@@ -48,6 +43,8 @@ export class PlaceController {
     @Query('page', new ParseOptionalNumberPipe(1)) page?: number,
     @Query('limit', new ParseOptionalNumberPipe(5)) limit?: number,
   ) {
+    if (!query) throw new BadRequestException('검색어를 입력해 주세요.');
+
     return this.placeService.getPlaces(query, page, limit);
   }
 
