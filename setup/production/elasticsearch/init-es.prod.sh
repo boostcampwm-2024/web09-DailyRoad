2488415886ac87de6b56e8d587e07682c34e08ef
miensoap <miensop51@gmail.com>
2024-11-30 15:53:29 +0900

    fix: ES 초기설정 스크립트 수정

--- setup/production/elasticsearch/init-es.prod.sh
+++ setup/production/elasticsearch/init-es.prod.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-# 현재 스크립트의 디렉터리 가져오기
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-INDEX_FILE="$SCRIPT_DIR/index/place-index.json"
-
-# Elasticsearch 상태 확인
-until curl -s -XGET "http://localhost:9200/_cluster/health" | grep '"status":"green"' > /dev/null; do
-  echo "Waiting for Elasticsearch to be ready..."
-  sleep 5
-done
-
 echo "Checking if 'place' index exists..."
 response=$(curl -s -o /dev/null -w "%{http_code}" -XGET "http://elasticsearch:9200/place" -u "elastic:${ELASTIC_PASSWORD}")
 if [ "$response" -eq 404 ]; then
@@ -17,7 +6,7 @@ if [ "$response" -eq 404 ]; then
   curl -X PUT "http://elasticsearch:9200/place" \
        -H "Content-Type: application/json" \
        -u "elastic:${ELASTIC_PASSWORD}" \
-       --data-binary @"$INDEX_FILE"
+       --data-binary @/usr/share/elasticsearch/place-index.json
   echo "'place' index created successfully."
 elif [ "$response" -eq 200 ]; then
   echo "Index 'place' already exists."
