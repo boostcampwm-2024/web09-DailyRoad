047927082b77116c4cc937bb1ef1160ea66eada4
miensoap <miensop51@gmail.com>
2024-12-01 14:53:16 +0900

    fix: 로그인 했을때 헤더 스타일 없는 오류 수정
    #preview

--- frontend/src/pages/HomePage/UserProfile.tsx
+++ frontend/src/pages/HomePage/UserProfile.tsx
@@ -6,7 +6,7 @@ type UserProfileProps = {
 
 const UserProfile = ({ user }: UserProfileProps) => {
   return (
-    <div className="flex items-center justify-between gap-1 rounded-lg border-[1px] border-c_button_gray p-2">
+    <div className="flex items-center justify-between gap-2 rounded-lg border-[1px] border-c_button_gray p-2">
       <img
         alt={`${user.nickname}님의 프로필 사진`}
         className="h-8 w-8 rounded-full"
