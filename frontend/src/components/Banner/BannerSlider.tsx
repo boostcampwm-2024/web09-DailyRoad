c8470e10c74bf758795024d65625a5157dbd1266
Soap <miensop51@gmail.com>
2024-12-01 14:18:39 +0900

    fix: react-slick 라이브러리 사용해서 문제 해결 #216
    #preview

--- frontend/src/components/Banner/BannerSlider.tsx
+++ frontend/src/components/Banner/BannerSlider.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { getBanners, Banner } from '@/api/banner';
-import styles from './BannerSlider.module.css';
-import BannerButton from './BannerButton';
 
 interface BannerSliderProps {
   className?: string;
@@ -13,116 +14,38 @@ const BannerSlider: React.FC<BannerSliderProps> = ({
   interval = 5000,
 }) => {
   const [banners, setBanners] = useState<Banner[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(1);
-  const sliderRef = useRef<HTMLDivElement>(null);
-  const isTransitioning = useRef(false);
-  const autoSlideInterval = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const fetchBanners = async () => {
       const data = await getBanners();
-      const lastBanner = data[data.length - 1];
-      const firstBanner = data[0];
-      setBanners([lastBanner, ...data, firstBanner]);
+      setBanners([...data]);
     };
 
     fetchBanners();
   }, []);
 
-  useEffect(() => {
-    if (banners.length > 0 && sliderRef.current) {
-      sliderRef.current.style.transition = 'none';
-      sliderRef.current.style.transform = `translateX(-100%)`;
-    }
-  }, [banners]);
-
-  useEffect(() => {
-    if (!sliderRef.current) return;
-
-    if (!isTransitioning.current) {
-      sliderRef.current.style.transition = 'transform 0.5s ease-in-out';
-    }
-    sliderRef.current.style.transform = `translateX(-${currentIndex * 100}%)`;
-
-    isTransitioning.current = true;
-  }, [currentIndex]);
-
-  const isToLastBanner = currentIndex === 0;
-  const isToFirstBanner = currentIndex === banners.length - 1;
-
-  const handleTransitionEnd = () => {
-    if (!sliderRef.current || banners.length === 0) return;
-
-    if (isToLastBanner) setCurrentIndex(banners.length - 2);
-    if (isToFirstBanner) setCurrentIndex(1);
-
-    isTransitioning.current = false;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: interval / 10,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: interval,
   };
-
-  const handleNext = () => {
-    if (isTransitioning.current) return;
-    setCurrentIndex((prevIndex) => prevIndex + 1);
-  };
-
-  const handlePrev = () => {
-    if (isTransitioning.current) return;
-    setCurrentIndex((prevIndex) => prevIndex - 1);
-  };
-
-  const startAutoSlide = () => {
-    if (autoSlideInterval.current) return;
-    autoSlideInterval.current = setInterval(() => {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
-    }, interval);
-  };
-
-  const stopAutoSlide = () => {
-    if (autoSlideInterval.current) {
-      clearInterval(autoSlideInterval.current);
-      autoSlideInterval.current = null;
-    }
-  };
-
-  useEffect(() => {
-    if (banners.length > 0) {
-      startAutoSlide();
-    }
-    return () => stopAutoSlide();
-  }, [banners]);
-
-  if (banners.length === 0) {
-    return (
-      <div className={`${styles.bannerWrap} ${className}`}>
-        <div>Loading...</div>
-      </div>
-    );
-  }
 
   return (
-    <div
-      className={`${styles.bannerWrap} ${className}`}
-      onMouseEnter={stopAutoSlide}
-      onMouseLeave={startAutoSlide}
-    >
-      <div
-        className={styles.rollingList}
-        ref={sliderRef}
-        onTransitionEnd={handleTransitionEnd}
-        style={{ width: `${banners.length * 100}%` }}
-      >
+    <div className={className}>
+      <Slider {...settings} className={'h-full w-full'}>
         {banners.map((banner, index) => (
-          <div className={styles.bannerItem} key={index}>
+          <div key={index}>
             <img
               src={banner.imageUrl}
               alt={`Banner ${index}`}
-              className={styles.bannerImage}
               onClick={() => window.open(banner.redirectUrl, '_blank')}
             />
           </div>
         ))}
-      </div>
-      <BannerButton direction="left" onClick={handlePrev} />
-      <BannerButton direction="right" onClick={handleNext} />
+      </Slider>
     </div>
   );
 };
