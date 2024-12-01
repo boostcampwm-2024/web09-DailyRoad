6ab852dcd5ddb951ddc00a6a8e7bf7a86c5c8dac
miensoap <miensop51@gmail.com>
2024-12-01 14:36:33 +0900

    feat: 로고 클릭하면 메인 페이지로 이동 #216

--- frontend/src/pages/HomePage/Header.tsx
+++ frontend/src/pages/HomePage/Header.tsx
@@ -2,12 +2,15 @@ import { Suspense } from 'react';
 
 import logo from '/logo.svg';
 import LoginButtons from './LoginButtons';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
-        <img src={logo} alt="Logo" className="h-8 w-auto" />
+        <Link to="/">
+          <img src={logo} alt="Logo" className="h-8 w-auto cursor-pointer" />
+        </Link>
         <Suspense fallback={<div>Loading...</div>}>
           <LoginButtons />
         </Suspense>
