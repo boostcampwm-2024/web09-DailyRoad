9d29b63dadc6c43324f928b7ab07f1b079fc6408
1119wj <1119wj@naver.com>
2024-12-02 01:04:43 +0900

    feat: 커스텀 마커 클러스터러, 커스텀 클러스터 알고리즘 #222

--- frontend/src/lib/CustomSuperCluseterAlgorithm.ts
+++ frontend/src/lib/CustomSuperCluseterAlgorithm.ts
@@ -1,10 +1,62 @@
 import {
+  AlgorithmInput,
+  AlgorithmOutput,
+  getPaddedViewport,
+  MarkerUtils,
   SuperClusterViewportAlgorithm,
   SuperClusterViewportOptions,
+  SuperClusterViewportState,
 } from '@googlemaps/markerclusterer';
+import equal from 'fast-deep-equal';
 
 export class CustomSuperClusterAlgorithm extends SuperClusterViewportAlgorithm {
   constructor({ ...options }: SuperClusterViewportOptions) {
     super(options);
+    this.clusters = [];
+  }
+
+  public calculate(input: AlgorithmInput): AlgorithmOutput {
+    const state: SuperClusterViewportState = {
+      zoom: Math.round(input.map.getZoom()!),
+      view: getPaddedViewport(
+        input.map.getBounds()!,
+        input.mapCanvasProjection,
+        this.viewportPadding,
+      ),
+    };
+
+    let changed = !equal(this.state, state);
+    if (!equal(input.markers, this.markers)) {
+      // TODO use proxy to avoid copy?
+      this.markers = [...input.markers];
+
+      const points = this.markers.map((marker) => {
+        const position = MarkerUtils.getPosition(marker);
+        const coordinates = [position.lng(), position.lat()];
+        return {
+          type: 'Feature' as const,
+          geometry: {
+            type: 'Point' as const,
+            coordinates,
+          },
+          properties: { marker },
+        };
+      });
+      this.superCluster.load(points);
+    }
+
+    const newClusters = this.cluster(input);
+    console.log(newClusters.length, this.clusters.length);
+    //this.clusters.length !== newClusters.length
+    //!equal(this.clusters, newClusters)
+    if (this.clusters.length !== newClusters.length) {
+      this.clusters = newClusters;
+    } else {
+      changed = false;
+    }
+
+    this.state = state;
+
+    return { clusters: this.clusters, changed };
   }
 }
