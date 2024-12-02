b90a550ae3bcd742304a6ef58b5149e5e4e8a966
miensoap <miensop51@gmail.com>
2024-12-02 23:04:47 +0900

    feat: 푸터가 화면에 항상 보이는 것이 아닌 가장 하단에 위치하도록 수정
    #preview

--- frontend/src/pages/HomePage/Footer.tsx
+++ frontend/src/pages/HomePage/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full bg-white py-2 text-gray-400">
+    <footer className="w-full bg-white py-4 text-gray-400 shadow-md">
       <div className="container mx-auto flex flex-col items-center">
-        <div className="mb-4 flex space-x-4">
+        <div className="mb-2 flex space-x-4">
           <a
             href="https://github.com/boostcampwm-2024/web09-DailyRoad"
-            className="hover:text-gray-400"
+            className="hover:text-gray-600"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -13,16 +13,17 @@ const Footer = () => {
           </a>
           <a
             href="https://elastic-bread-9ef.notion.site/12963e6f4ee98074b6f9f70cfa9ac836"
-            className="hover:text-gray-400"
+            className="hover:text-gray-600"
             target="_blank"
             rel="noopener noreferrer"
           >
             Notion
           </a>
         </div>
-        <p className="text-sm">2024 DailyRoad. All rights reserved.</p>
+        <p className="text-xs">2024 DailyRoad. All rights reserved.</p>
       </div>
     </footer>
   );
 };
+
 export default Footer;
