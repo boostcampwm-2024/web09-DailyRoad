78e5dd7b33f43b2f0216518f859a3a0bed686897
miensoap <miensop51@gmail.com>
2024-12-02 23:04:22 +0900

    fix: 로고를 클릭시 href 사용하도록 수정

--- frontend/src/pages/HomePage/Header.tsx
+++ frontend/src/pages/HomePage/Header.tsx
@@ -2,15 +2,18 @@ import { Suspense } from 'react';
 
 import logo from '../../assets/logo.svg';
 import LoginButtons from './LoginButtons';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
-        <Link to="/">
-          <img src={logo} alt="Logo" className="h-8 w-auto cursor-pointer" />
-        </Link>
+        <img src={logo}
+             alt="Logo"
+             className="h-8 w-auto cursor-pointer"
+             onClick={() => {
+               window.location.href = '/';
+             }}>
+        </img>
         <Suspense fallback={<div>Loading...</div>}>
           <LoginButtons />
         </Suspense>
