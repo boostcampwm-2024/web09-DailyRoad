30981d048d1502207b73704b5a1e1ceb5022e06c
1119wj <1119wj@naver.com>
2024-12-05 18:18:47 +0900

    feat: map 탭, 썸네일 크기 조정 #221

--- frontend/src/components/common/List/ListToggleButtons.tsx
+++ frontend/src/components/common/List/ListToggleButtons.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import { useStore } from '@/store/useStore';
 
-interface ToggleButtonProps {
+type ToggleButtonProps = {
   options: { value: string; label: string }[];
   selected: string;
   onSelect: (value: string) => void;
-}
+};
 
-const ToggleButton: React.FC<ToggleButtonProps> = ({
-  options,
-  selected,
-  onSelect,
-}) => {
+const ToggleButton = ({ options, selected, onSelect }: ToggleButtonProps) => {
+  const addToast = useStore((state) => state.addToast);
+  const isLogged = useStore((state) => state.isLogged);
+  const onClick = (option: string) => {
+    if (option === 'MY' && !isLogged) {
+      addToast('로그인 후 이용해주세요', '', 'error');
+      return;
+    }
+    onSelect(option);
+  };
+
   return (
-    <div className="relative flex h-[45px] w-[700px] rounded-md bg-gray-200 shadow-inner">
+    <div className="relative mb-8 flex h-[45px] w-[700px] rounded-md bg-gray-200 shadow-inner">
       <div
         className={`absolute left-0 top-0 h-full w-1/2 rounded-md bg-c_bg_blue transition-transform duration-300 ${
           selected === options[1].value ? 'translate-x-full' : ''
@@ -25,7 +31,7 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({
           className={`test relative z-10 h-full w-1/2 text-center text-lg font-medium ${
             selected === option.value ? 'text-white' : 'text-gray-700'
           }`}
-          onClick={() => onSelect(option.value)}
+          onClick={() => onClick(option.value)}
         >
           {option.label}
         </button>
