a5f21b31d1400556b0934150b7ec11adc190f776
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-12-05 15:37:52 +0900

    refactor: 주석 제거 #241

--- backend/src/common/consts/index.ts
+++ backend/src/common/consts/index.ts
@@ -1,3 +1,2 @@
-// Todo. 오브젝트 스토리지에 실제 이미지 저장 후 수정
 export const DEFAULT_THUMBNAIL_URL =
   'https://kr.object.ncloudstorage.com/ogil-public/post/uploads/35af1797a54a.jpg';
--- backend/src/course/entity/Course.ts
+++ backend/src/course/entity/Course.ts
@@ -7,7 +7,6 @@ import { PlaceInCourseNotFoundException } from '@src/course/exception/PlaceInCou
 
 @Entity()
 export class Course extends BaseEntity {
-  // Todo. 오브젝트 스토리지에 실제 이미지 저장 후 수정
   public static readonly DEFAULT_THUMBNAIL_URL =
     'https://kr.object.ncloudstorage.com/ogil-public/post/uploads/35af1797a54a.jpg';
 
