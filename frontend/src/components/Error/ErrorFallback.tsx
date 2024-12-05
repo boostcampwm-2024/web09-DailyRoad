7d2a7a075e574b19a9a994e8f0a03e45cb336657
1119wj <1119wj@naver.com>
2024-12-05 18:26:01 +0900

    fix: 메인페이지 이동 #221
    #preview

--- frontend/src/components/Error/ErrorFallback.tsx
+++ frontend/src/components/Error/ErrorFallback.tsx
@@ -1,6 +1,3 @@
-import { ROUTES } from '@/constants/routes';
-import { useNavigate } from 'react-router-dom';
-
 export type ErrorProps = {
   statusCode?: number;
   errorCode?: number;
@@ -8,13 +5,14 @@ export type ErrorProps = {
 };
 
 const ErrorFallback = ({ statusCode, errorCode, resetError }: ErrorProps) => {
-  const navigate = useNavigate();
   return (
     <div role="alert" style={{ padding: '20px', textAlign: 'center' }}>
       <h2>문제가 발생했습니다.</h2>
       {statusCode && <p>상태 코드: {statusCode}</p>}
       {errorCode && <p>에러 코드: {errorCode}</p>}
-      <button onClick={() => navigate(ROUTES.ROOT)}>메인으로 이동하기</button>
+      <button onClick={() => (window.location.href = '/')}>
+        메인으로 이동하기
+      </button>
     </div>
   );
 };
