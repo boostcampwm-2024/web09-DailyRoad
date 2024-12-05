4f3a4cf6397fc8cec3743c4663c0f0a3f12fa1b5
hyohyo12 <129946082+hyohyo12@users.noreply.github.com>
2024-12-05 15:28:07 +0900

    fix: 지도/코스 기본 썸네일 변경 #241

--- backend/src/course/entity/Course.ts
+++ backend/src/course/entity/Course.ts
@@ -9,7 +9,7 @@ import { PlaceInCourseNotFoundException } from '@src/course/exception/PlaceInCou
 export class Course extends BaseEntity {
   // Todo. 오브젝트 스토리지에 실제 이미지 저장 후 수정
   public static readonly DEFAULT_THUMBNAIL_URL =
-    'https://avatars.githubusercontent.com/u/87180146?v=4';
+    'https://kr.object.ncloudstorage.com/ogil-public/post/uploads/35af1797a54a.jpg';
 
   @ManyToOne(() => User, (user) => user.courses, { eager: true })
   @JoinColumn({ name: 'user_id' })
